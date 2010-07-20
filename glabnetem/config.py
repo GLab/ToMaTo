491a0544d65d92417455a6cda25b8fa05b55257a
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2010-07-20 21:34:16 +0200

    Config files

--- glabnetem/config.py
+++ glabnetem/config.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
-class Config(object):
+from util import *
 
-	default_template = "debian"
-	local_deploy_dir = "deploy"
-	remote_deploy_dir = "/root/glabnetem"
-	topology_dir = "topologies"
+import ConfigParser, os
+
+class ConfigHelper(object):
+	config=ConfigParser.SafeConfigParser()
+	config.read(['glabnetem.conf', '/etc/glabnetem.conf', os.path.expanduser('~/.glabnetem.conf')])
+	
+class Config(object):
+	def get(section,option,default):
+		if ConfigHelper.config.has_option(section, option): 
+			return ConfigHelper.config.get(section, option)
+		else:
+			return default
+	get = static(get)
+	
+	local_deploy_dir = get("local","deploy_dir","/tmp/glabnetem")
+	topology_dir = get("local","topology_dir","topologies")
+	default_template = get("openvz","default_template","debian")
+	remote_deploy_dir = get("remote","deploy_dir","/root/glabnetem")
