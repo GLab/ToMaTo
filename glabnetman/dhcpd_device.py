237d4ce5ead2aed84292c7579cb781ffb3ce55f8
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2010-07-23 14:55:03 +0200

    More documentation

--- glabnetman/dhcpd_device.py
+++ glabnetman/dhcpd_device.py
@@ -11,10 +11,31 @@ class DhcpdDevice(Device):
 	"""
   
 	subnet=property(curry(Device.get_attr,"subnet"),curry(Device.set_attr,"subnet"))
+	"""
+	The subnet-number should be an IP address or domain name which resolves to the subnet number of the subnet being described. [dhcpd.conf.5]
+	"""
+	
 	netmask=property(curry(Device.get_attr,"netmask"),curry(Device.set_attr,"netmask"))
+	"""
+	The  netmask  should  be  an  IP address or domain name which resolves to the subnet mask of the subnet being described. The subnet number, together with the netmask, are sufficient to determine whether any given IP address is on the specified subnet. [dhcpd.conf.5]
+	"""
 	range=property(curry(Device.get_attr,"range"),curry(Device.set_attr,"range"))
+	"""
+	Syntax: low-address [high-address]
+	The range statement gives the lowest  and  highest IP addresses in a range. All IP addresses in the range should be in the subnet in which the range statement is declared. [...] When specifying a single address, high-address can be omitted. [dhcpd.conf.5]
+	"""
+	
 	gateway=property(curry(Device.get_attr,"gateway"),curry(Device.set_attr,"gateway"))
+	"""
+	Syntax: ip-address [, ip-address...  ];
+	The routers option specifies a list of IP addresses for routers on the client’s subnet. Routers should be listed in order of preference. [dhcp-options.5]
+	"""
+	
 	nameserver=property(curry(Device.get_attr,"nameserver"),curry(Device.set_attr,"nameserver"))
+	"""
+	Syntax: ip-address [, ip-address...  ];
+	The  domain-name-servers  option specifies a list of Domain Name System (STD 13, RFC 1035) name servers available to the client.  Servers should be listed in order of preference. [dhcp-options.5]
+	"""
 
 	def retake_resources(self):
 		"""
@@ -58,7 +79,7 @@ class DhcpdDevice(Device):
 		dhcpd_fd.write("  range %s;\n" % self.range )
 		dhcpd_fd.write("}\n" )
 		start_fd=open(self.topology.get_deploy_script(self.host_name,"start"), "a")
-		start_fd.write("dhcpd -cf dhcpd.%s.conf -pf %s.pid -lf leases" % ( self.id, self.id ) )
+		start_fd.write("dhcpd3 -cf dhcpd.%s.conf -pf %s.pid -lf leases" % ( self.id, self.id ) )
 		for iface in self.interfaces.values():
 			start_fd.write(" %s" % self.bridge_name(iface))
 		start_fd.write(" &\n")
