76649f7581b9956e9fcf0d05a30b5b1385b25d8d
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2010-08-19 10:37:35 +0200

    Added tinc router support

--- backend/glabnetman/tinc.py
+++ backend/glabnetman/tinc.py
@@ -2,7 +2,7 @@
 
 from django.db import models
 
-import dummynet, generic, hosts, os, subprocess, shutil, fault
+import dummynet, generic, hosts, os, subprocess, shutil, fault, util
 
 class TincConnector(generic.Connector):
 	
@@ -37,9 +37,21 @@ class TincConnector(generic.Connector):
 			con.emulatedconnection.tincconnection.start(task)
 			tincname = self.tincname(con)
 			host.execute ( "tincd --net=%s" % tincname, task)
-			#FIXME: brctl does not work for routing
-			host.execute ( "brctl addif %s %s" % (con.bridge_name(), tincname ), task)
 			host.execute ( "ip link set %s up" %  tincname, task)
+			if self.type == "router":
+				table_in = 1000 + 2 * con.id
+				table_out = 1000 + 2 * con.id + 1 
+				host.execute ( "grep '^%s ' /etc/iproute2/rt_tables || echo \"%s %s\" >> /etc/iproute2/rt_tables" % ( table_in, table_in, table_in ), task )
+				host.execute ( "grep '^%s ' /etc/iproute2/rt_tables || echo \"%s %s\" >> /etc/iproute2/rt_tables" % ( table_out, table_out, table_out ), task )
+				host.execute ( "iptables -t mangle -A PREROUTING -i %s -j MARK --set-mark %s" % ( tincname, table_in ), task )
+				host.execute ( "iptables -t mangle -A PREROUTING -i %s -j MARK --set-mark %s" % ( con.bridge_name(), table_out ), task )
+				host.execute ( "ip rule add fwmark %s table %s" % ( hex(table_in), table_in ), task )
+				host.execute ( "ip rule add fwmark %s table %s" % ( hex(table_out), table_out ), task )
+				host.execute ( "ip route add table %s default dev %s" % ( table_in, con.bridge_name() ), task )
+				host.execute ( "ip route add table %s default dev %s" % ( table_out, tincname ), task )
+				host.execute ( "ifconfig %s %s netmask %s up" % (con.bridge_name(), con.upcast().gateway_ip, con.upcast().gateway_netmask), task )
+			else:
+				host.execute ( "brctl addif %s %s" % (con.bridge_name(), tincname ), task)
 
 	def stop(self, task):
 		generic.Connector.stop(self, task)
@@ -47,6 +59,15 @@ class TincConnector(generic.Connector):
 			host = con.interface.device.host
 			con.emulatedconnection.tincconnection.stop(task)
 			tincname = self.tincname(con)
+			if self.type == "router":
+				table_in = 1000 + 2 * con.id
+				table_out = 1000 + 2 * con.id + 1 
+				host.execute ( "iptables -t mangle -D PREROUTING -i %s -j MARK --set-mark %s" % ( tincname, table_in ), task )
+				host.execute ( "iptables -t mangle -D PREROUTING -i %s -j MARK --set-mark %s" % ( con.bridge_name(), table_out ), task )
+				host.execute ( "ip rule del fwmark %s table %s" % ( hex(table_in), table_in ), task )
+				host.execute ( "ip rule del fwmark %s table %s" % ( hex(table_out), table_out ), task )
+				host.execute ( "ip route del table %s default dev %s" % ( table_in, con.bridge_name() ), task )
+				host.execute ( "ip route del table %s default dev %s" % ( table_out, tincname ), task )
 			host.execute ( "cat /var/run/tinc.%s.pid | xargs kill" % tincname, task)
 			host.execute ( "rm /var/run/tinc.%s.pid" % tincname, task)
 
@@ -137,6 +158,8 @@ class TincConnector(generic.Connector):
 
 class TincConnection(dummynet.EmulatedConnection):
 	tinc_port = models.IntegerField()
+	gateway_ip = models.CharField(max_length=15, null=True) 
+	gateway_netmask = models.CharField(max_length=15, null=True)
 	
 	def init(self, connector, dom):
 		self.connector = connector
@@ -150,11 +173,17 @@ class TincConnection(dummynet.EmulatedConnection):
 
 	def encode_xml(self, dom, doc, internal):
 		dummynet.EmulatedConnection.encode_xml(self, dom, doc, internal)
+		if self.gateway_ip:
+			dom.setAttribute("gateway_ip", self.gateway_ip)
+		if self.gateway_netmask:
+			dom.setAttribute("gateway_netmask", self.gateway_netmask)
 		if internal:
 			dom.setAttribute("tinc_port", self.tinc_port)
 		
 	def decode_xml(self, dom):
 		dummynet.EmulatedConnection.decode_xml(self, dom)
+		self.gateway_ip = util.get_attr(dom, "gateway_ip", default=None)
+		self.gateway_netmask = util.get_attr(dom, "gateway_netmask", default=None)
 		
 	def start(self, task):
 		dummynet.EmulatedConnection.start(self, task)
