04597b04b6a8491be4394479d76c5a35cf7faa98
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2011-05-05 12:07:46 +0200

    Script to upload deb files

---
+++ dist/upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+HOST=root@fileserver.german-lab.de
+DEBS=/root/debian-packages
+REPO=/data/files/debian
+
+function upload () {
+  PKG=$1
+  DISTRO=$2
+  for i in $(find $PKG -name *.deb); do
+    scp $i $HOST:$DEBS
+    ssh $HOST reprepro -b $REPO includedeb $DISTRO $DEBS/$(basename $i)
+  done
+}
+
+upload dummynet lenny
+upload hostserver lenny
+upload kernel lenny
+upload meta lenny
+upload backend squeeze
+upload web squeeze
