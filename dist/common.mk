a7965bc59b23aca325e8119949e5a323f00721d9
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2011-06-23 16:37:45 +0200

    Updated kernel package

--- dist/common.mk
+++ dist/common.mk
@@ -1,4 +1,11 @@
+.PHONY: check-build-deps-check-deps
+check-build-deps-check-deps:
+	@touch no-missing-deps
+	@$(foreach DEP, $(DEPENDENCIES), \
+	  dpkg-query -s $(DEP) 2>/dev/null | fgrep 'install ok installed' >/dev/null \
+	    || (echo "Please install the following packages and re-run make: $(DEP)"; \
+                [ -f no-missing-deps ] && rm no-missing-deps); \
+        )
+
 .PHONY: check-build-deps
-check-build-deps:
-	@dpkg-query -s $(DEPENDENCIES) > /dev/null 2>&1 \
-		|| (echo "Please install the following packages and re-run make: $(DEPENDENCIES)"; exit 1)
+check-build-deps: check-build-deps-check-deps no-missing-deps
