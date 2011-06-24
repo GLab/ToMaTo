4243626736118e76773b3a5d60211f0de0b86f02
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2011-06-24 11:57:12 +0200

    Small changes to package build system

--- dist/common.mk
+++ dist/common.mk
@@ -1,4 +1,59 @@
-.PHONY: check-build-deps-check-deps
+VERSION?=$(_VERSION)
+SUBVERSION?=$(_SUBVERSION)
+VER=$(VERSION)$(SUBVERSION)
+DEBNAME=$(PACKAGE)_$(VER)
+DIR=$(PACKAGE)-$(VER)
+ORIG=$(PACKAGE)_$(VERSION).orig.tar.gz
+DEBFILE=$(DEBNAME)_*.deb
+CHANGES=debian.tar.gz
+
+default: build
+
+unpack: deb-unpack
+
+deb-unpack: $(ORIG) $(CHANGES)
+	tar -xzf $(ORIG)
+	tar -xzf $(CHANGES) -C $(DIR)
+	(cd $(DIR); QUILT_PATCHES=debian/patches QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index" quilt push -a; cd ..)
+
+$(DIR): deb-unpack
+
+pack: deb-pack
+
+deb-pack:
+	(cd $(DIR); debuild clean; cd ..)
+	dpkg-source --format="3.0 (quilt)" --compression=gzip -b $(DIR)
+	mv $(DEBNAME).$(CHANGES) $(CHANGES)
+	rm $(DEBNAME).dsc
+
+
+build: $(DEBFILE)
+
+$(DEBFILE): $(DIR)
+	(cd $(DIR); debuild -b -us -uc; cd ..)
+
+init: deb-init
+
+deb-init: $(ORIG) create_debian deb-pack
+
+create_debian:
+	tar -xzf $(ORIG)
+	(cd $(DIR); dh_make -n ; cd ..)
+
+clean: deb-clean
+
+deb-clean:
+	rm -rf $(DIR)
+	rm -f $(DEBNAME)_*.*
+
+deb-release: deb-pack deb-clean version deb-unpack dev-changelog deb-build
+deb-changelog: $(DIR)
+	(cd $(DIR); dch -v $(VER); cd ..)	
+
+version:
+	sed -i 's/$(_VERSION)/$(VERSION)/g' Makefile
+	sed -i 's/$(_SUBVERSION)//g' Makefile
+
 check-build-deps-check-deps:
 	@touch no-missing-deps
 	@$(foreach DEP, $(DEPENDENCIES), \
@@ -7,5 +62,7 @@ check-build-deps-check-deps:
                 [ -f no-missing-deps ] && rm no-missing-deps); \
         )
 
-.PHONY: check-build-deps
-check-build-deps: check-build-deps-check-deps no-missing-deps
+check-build-deps-cleanup:
+	@rm no-missing-deps
+
+check-build-deps: check-build-deps-check-deps no-missing-deps check-build-deps-cleanup
