0fa4f6bab8eb7261554ea9def27366b46bfd45ec
sylar5 <tim.tg.gerhard@googlemail.com>
2012-10-19 16:12:59 +0200

    Added "Sites" to admin menu, which shows a sites list

---
+++ web/tomato/site.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# ToMaTo (Topology management software) 
+# Copyright (C) 2012 Tim Gerhard, University of Kaiserslautern
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.http import Http404
+from django import forms
+from django.core.urlresolvers import reverse
+
+from lib import *
+import xmlrpclib
+
+@wrap_rpc
+def index(api, request):
+    return render_to_response("admin/site/index.html", {'site_list': api.site_list()})
