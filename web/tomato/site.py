33ded73270a3d08421b9d24e68a4a4559fdf876a
Tim Gerhard <tim.tg.gerhard@googlemail.com>
2012-10-22 10:07:58 +0200

    updated copyright notice

--- web/tomato/site.py
+++ web/tomato/site.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # ToMaTo (Topology management software) 
-# Copyright (C) 2012 Tim Gerhard, University of Kaiserslautern
+# Copyright (C) 2012 Integrated Communication Systems Lab, University of Kaiserslautern
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of the ToMaTo project
 #
+# ToMaTo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -52,7 +54,7 @@ def add(api, request):
         form = AddSiteForm(request.POST)
         if form.is_valid():
             formData = form.cleaned_data
-            if formData["name"]: #At this point: check if trying to add a duplicate.
+            if formData["name"]: #At this point, check if trying to add a duplicate.
                 api.site_create(formData["name"],formData["description"])
                 return render_to_response("admin/site/add_success.html", {'name': formData["name"]})
             else:
