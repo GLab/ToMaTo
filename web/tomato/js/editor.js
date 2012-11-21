c68d4c561744a80c7e19d44d7fec754562bd603b
Tim Gerhard <tim.tg.gerhard@googlemail.com>
2012-11-21 23:13:10 +0100

    Modified tutorial appearance and code structure. The tutorial doesn't accept any progress by now; it just shows a static message.

--- web/tomato/js/editor.js
+++ web/tomato/js/editor.js
@@ -369,8 +369,47 @@ var Workspace = Class.extend({
     	this.canvas.relPos = function(pos) {
     		return {x: (pos.x - fs) / (c.width-2*fs), y: (pos.y - fs) / (c.height-2*fs)};
     	}
-    	this.beginnerHelp = this.canvas.text(c.width/2, c.height/4, "").attr({"font-size": 25, "fill": "#999999"});
-    	this.updateBeginnerHelp();
+    	
+    	this.tutorialBarBG = this.canvas.rect(0,c.height-120,c.width,120,0).attr({"fill":"#660000"});
+    	this.tutorialBarText = this.canvas.text(c.width/2, c.height-60, "").attr({"font-size": 13, "fill": "#ffffff"});
+    	
+    	this.tutorialStatus = 3;
+    	this.tutorialSentences = [
+    	
+    		//0
+    		"Welcome to ToMaTo!. You have just created a new Topology.\n\
+    		This guide will tell you the basics of how to use this editor.\n\
+    		If you already know how to use this tool, you can disable this tutorial at any point by disabling 'Beginner mode' in 'Options'.\n \n\
+    		To add a first device to your topology, first click on 'OpenVZ' (blue screen) in 'Common elements' in the menu above, and then\n\
+    		click somewhere in the white field.",
+    		
+    		//1
+    		"Congratulations! You have placed your first OpenVZ device.\n\
+    		You can always identify OpenVZ devices by a blue screen.\n\
+    		OpenVZ devices are virtual machines which use their host's kernel to operate, but have their own virtual file system.\n\
+    		This makes them more efficient to run, but disallows modifying the kernel.\n\
+    		This also means, you can only run Linux systems in OpenVZ devices.\n \n\
+    		You can move your new device via drag-and-drop. Try it!",
+    		
+    		//2
+    		"You will need more devices to get a whole topology. This time, let's create a KVM device.\n \n\
+    		Click 'KVM' in 'Common elements' in the menu above, and the place it in the editor by clicking somewhere into the white.",
+    		
+    		//3
+    		"You just created a KVM device.\n\
+    		KVM devices can be identified by a green screen.\n\
+    		Contary to OpenVZ, KVM devices run completely separate from their host systems.\n\
+    		This means that you can modify the kernel and/or use any system which supports the host's processor architecture.\n \n\
+    		By now, the two devices don't have any network connection.\n\
+    		To connect them, right-Click on one of them, select 'Connect', and then left-click on the other one.",
+    		
+    		//4
+    		"You have connected the two devices."
+    		
+    	]
+    	
+    	this.updateTutorialBar();
+    	
 		var t = this;
 		this.connectPath = this.canvas.path("M0 0L0 0").attr({"stroke-dasharray": "- "});
 		this.container.click(function(evt){
@@ -380,25 +419,35 @@ var Workspace = Class.extend({
 			t.onMouseMove(evt);
 		});
 	},
-	updateBeginnerHelp: function() {
+	
+	setTutorialText: function(text) { //set tutorial text to 'text'.
+		this.tutorialBarText.attr({text: text});
+	},
+	setVisibleTutorial: function(vis) {  //vis==true: show tutorial. vis==false: hide tutorial.
+		if (vis) {
+			this.tutorialBarBG.show();
+			this.tutorialBarText.show();
+		} else {
+			this.tutorialBarBG.hide();
+			this.tutorialBarText.hide();
+		}
+	},
+	
+	updateTutorialBar: function() {
+	
+		//just hide the tutorial when disabled.
 		if (! this.editor.options.beginner_mode) {
-			this.beginnerHelp.hide();
+			this.setVisibleTutorial(false);
 			return;
 		}
-		var text = "";
-		if (this.editor.topology.elementCount() == 0) { 
-			text = "Select an element from the palette\nand click into the workspace to\nadd it to the topology.";
-		} else if (this.editor.topology.elementCount() < 3) { 
-			text = "Add some other elements to the topology\nso you can create connections.";
-		} else if (this.editor.topology.connectionCount() == 0) {
- 			text = "Right-click on an element, select connect\nand click on another element to\nconnect these elements.";
-		} else if (this.editor.topology.connectionCount() < 2) { 
-			text = "Add another connection so your\ntopology is fully connected.";
-		}
-		this.beginnerHelp.attr({text: text});
-		if (text) this.beginnerHelp.show();
-		else this.beginnerHelp.hide();
-		window.setTimeout("editor.workspace.updateBeginnerHelp();", 1000);
+		
+		//get text by current status, and insert it. If text is empty, hide the tutorial.
+		var text = this.tutorialSentences[this.tutorialStatus];
+		this.setTutorialText(text);
+		this.setVisibleTutorial(true);
+		
+		//wait a second, then run this again.
+		window.setTimeout("editor.workspace.updateTutorialBar();", 1000);
 	},
 	onMouseMove: function(evt) {
 		if (! this.editor.connectElement) {
@@ -426,7 +475,7 @@ var Workspace = Class.extend({
 		}
 	},
 	onOptionChanged: function(name) {
-    	this.updateBeginnerHelp();
+    	this.updateTutorialBar();
 	},
 	onModeChanged: function(mode) {
 		for (var name in Mode) this.container.removeClass("mode_" + Mode[name]);
