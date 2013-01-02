4b52010484b6d0ed064492f60729382637032bad
Tim Gerhard <tim.tg.gerhard@googlemail.com>
2013-01-02 23:40:37 +0100

    completed implementation of external networks admin

--- web/tomato/external_networks.py
+++ web/tomato/external_networks.py
@@ -27,7 +27,18 @@ from lib import *
 import xmlrpclib
 from admin_common import RemoveResourceForm, is_hostManager
 
-
+class NetworkForm(forms.Form):
+    kind = forms.CharField(label="Kind",max_length=255)
+    label = forms.CharField(max_length=255,label="Label",help_text="Visible Name")
+    preference = forms.IntegerField(label="Preference", help_text="The item with the highest preference will be the default one. An integer number.")
+   
+class EditNetworkForm(NetworkForm):
+    res_id = forms.CharField(max_length=50, widget=forms.HiddenInput)
+    def __init__(self, *args, **kwargs):
+        super(EditNetworkForm, self).__init__(*args, **kwargs)
+        self.fields["kind"].widget=forms.TextInput(attrs={'readonly':'readonly'})
+        self.fields["kind"].help_text=None
+    
 @wrap_rpc
 def index(api, request):
     netw_list = api.resource_list('network')
@@ -35,12 +46,78 @@ def index(api, request):
 
 @wrap_rpc
 def add(api, request):
-    return render_to_response("main/error.html",{'user': api.user, 'type':'not implemented yet'})
-   
+    if request.method == 'POST':
+        form = NetworkForm(request.POST)
+        if form.is_valid():
+            formData = form.cleaned_data
+            api.resource_create('network',{'kind':formData['kind'],
+                                           'label':formData['label'],
+                                           'preference':formData['preference']})
+           
+            return render_to_response("admin/external_networks/add_success.html", {'user': api.user, 'label': formData["label"]})
+        else:
+            return render_to_response("admin/external_networks/form.html", {'user': api.user, 'form': form, "edit":False})
+    else:
+        form = NetworkForm
+        return render_to_response("admin/external_networks/form.html", {'user': api.user, 'form': form, "edit":False,})
+
+
 @wrap_rpc
 def remove(api, request):
-    return render_to_response("main/error.html",{'user': api.user, 'type':'not implemented yet'})
+    if request.method == 'POST':
+        form = RemoveResourceForm(request.POST)
+        if form.is_valid():
+            res_id = form.cleaned_data["res_id"]
+            if api.resource_info(res_id) and api.resource_info(res_id)['type'] == 'network':
+                label = api.resource_info(res_id)['attrs']['label']
+                api.resource_remove(res_id)
+                return render_to_response("admin/external_networks/remove_success.html", {'user': api.user, 'label':label})
+            else:
+                return render_to_response("main/error.html",{'user': api.user, 'type':'invalid id','text':'There is no external network with id '+res_id})
+        else:
+            res_id = request.POST['res_id']
+            if res_id:
+                form = RemoveResourceForm()
+                form.fields["res_id"].initial = res_id
+                return render_to_response("admin/external_networks/remove_confirm.html", {'user': api.user, 'label': api.resource_info(res_id)['attrs']['label'], 'hostManager': is_hostManager(api.account_info()), 'form': form})
+            else:
+                return render_to_response("main/error.html",{'user': api.user, 'type':'Transmission Error','text':'There was a problem transmitting your data.'})
+    
+    else:
+        res_id = request.GET['id']
+        if res_id:
+            form = RemoveResourceForm()
+            form.fields["res_id"].initial = res_id
+            return render_to_response("admin/external_networks/remove_confirm.html", {'user': api.user, 'label': api.resource_info(res_id)['attrs']['label'], 'hostManager': is_hostManager(api.account_info()), 'form': form})
+        else:
+            return render_to_response("main/error.html",{'user': api.user, 'type':'not enough parameters','text':'No resource specified. Have you followed a valid link?'})
+    
 
 @wrap_rpc
 def edit(api, request):
-    return render_to_response("main/error.html",{'user': api.user, 'type':'not implemented yet'})
+    if request.method=='POST':
+        form = EditNetworkForm(request.POST)
+        if form.is_valid():
+            formData = form.cleaned_data
+            if api.resource_info(formData['res_id'])['type'] == 'network':
+                api.resource_modify(formData["res_id"],{'label':formData['label'],
+                                                        'preference':formData['preference']})
+                return render_to_response("admin/external_networks/edit_success.html", {'user': api.user, 'label': formData["label"], 'res_id': formData['res_id'], 'edited_data': True})
+            else:
+                return render_to_response("main/error.html",{'user': api.user, 'type':'invalid id','text':'The resource with id '+formData['res_id']+' is no external network.'})
+        else:
+            kind = request.POST["kind"]
+            if kind:
+                return render_to_response("admin/external_networks/form.html", {'user': api.user, 'label': kind, 'form': form, "edit":True, 'edit_data':True})
+            else:
+                return render_to_response("main/error.html",{'user': api.user, 'type':'Transmission Error','text':'There was a problem transmitting your data.'})
+    else:
+        res_id = request.GET['id']
+        if res_id:
+            res_info = api.resource_info(res_id)
+            origData = res_info['attrs']
+            origData['res_id'] = res_id
+            form = EditNetworkForm(origData)
+            return render_to_response("admin/external_networks/form.html", {'user': api.user, 'label': res_info['attrs']['label'], 'form': form, "edit":True, 'edit_data':True})
+        else:
+            return render_to_response("main/error.html",{'user': api.user, 'type':'not enough parameters','text':'No address specified. Have you followed a valid link?'})
