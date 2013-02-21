cf3b8417e5c4c7cf17dc2313c916068816e9f434
Tim Gerhard <tim.tg.gerhard@googlemail.com>
2013-02-21 20:56:57 +0100

    included map from tomato2. Removed dependency on old data scheme (currently does not show any data). Added support for site.description.

--- web/tomato/physical_link_stats.py
+++ web/tomato/physical_link_stats.py
@@ -23,7 +23,60 @@ from django.core.urlresolvers import reverse
 from lib import *
 import xmlrpclib, settings
 
+def get_site_location(site_name,api):
+	
+	#return api.site_info(site_name)['geolocation']
+	
+	if site_name=="ukl":
+		return {'longitude':7.768889,'latitude':49.444722}
+	if site_name=="tum":
+		return {'longitude':11.575278,'latitude':48.136944}
+	if site_name=="uwue":
+		return {'longitude':9.929444,'latitude':49.794444}
+	return {'longitude':0,'latitude':0}
+
+def site_list(api):
+	r = []
+	for i in api.site_list():
+		r.append(i['name'])
+	return r
+
+def site_location_list(api):
+	r = []
+	l = site_list(api)
+	for i in l:
+		r.append({'name':i,
+				  'geolocation':get_site_location(i,api),
+				  'displayName':api.site_info(i)['description']})
+	return r
+
+def site_site_pairs(api,allow_self=True): # allow_self: allow self-referencing pairs like ('ukl','ukl')
+	r = []
+	l = site_list(api)
+	for i in range(len(l)):
+		i_mod = i
+		if not (allow_self):
+			i_mod = i_mod+1
+		for j in range(i_mod,len(l)):
+			r.append((l[i],l[j]))
+	return r
+
+def site_site_connections(api):
+	r = []
+	l = site_site_pairs(api,False)
+	for i in l:
+		src = i[0]
+		dst = i[1]
+		
+		#TODO: add stats
+		
+		r.append({
+			"src":src,
+			"dst":dst
+			})
+	return r
+
 @wrap_rpc
 def index(api, request):
-	return render_to_response("admin/physical_link_stats/index.html",{'api':api})
+	return render_to_response("admin/physical_link_stats/index.html",{'site_location_list':site_location_list(api),'connections': site_site_connections(api),'user':api.account_info()})
 
