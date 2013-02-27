7b3e41d74d661bc30c7bc2107a00f36573e8e290
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2013-02-27 14:41:31 +0100

    Fixed repy prepare and destroy

--- backend/tomato/elements/repy.py
+++ backend/tomato/elements/repy.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from .. import elements
+from .. import elements, host, fault
 import generic
 
 class Repy(generic.VMElement):
@@ -25,10 +25,31 @@ class Repy(generic.VMElement):
 		"repy_interface": [generic.ST_CREATED, generic.ST_PREPARED],
 	}
 	PROFILE_ATTRS = ["ram", "diskspace", "cpus", "bandwidth"]
-	
 	class Meta:
 		db_table = "tomato_repy"
 		app_label = 'tomato'
+	def action_prepare(self):
+		hPref, sPref = self.getLocationPrefs()
+		_host = host.select(site=self.site, elementTypes=[self.TYPE]+self.CAP_CHILDREN.keys(), hostPrefs=hPref, sitePrefs=sPref)
+		fault.check(_host, "No matching host found for element %s", self.TYPE)
+		attrs = self._remoteAttrs()
+		attrs.update({
+			"template": self._template().name,
+		})
+		attrs.update(self._profileAttrs())
+		self.element = _host.createElement(self.TYPE, parent=None, attrs=attrs, owner=self)
+		self.save()
+		for iface in self.getChildren():
+			iface._create()
+		self.setState(generic.ST_PREPARED, True)
+	def action_destroy(self):
+		if self.element:
+			for iface in self.getChildren():
+				iface._remove()
+			self.element.remove()
+			self.element = None
+		self.setState(generic.ST_CREATED, True)
+
 	
 class Repy_Interface(generic.VMInterface):
 	TYPE = "repy_interface"
