c563ec3caf0feeb4f79d4bfc84b520b44ccd58f6
Tim Gerhard <tim.tg.gerhard@googlemail.com>
2013-03-13 12:01:58 +0100

    implemented colors in physical link map

--- web/tomato/physical_link_stats.py
+++ web/tomato/physical_link_stats.py
@@ -18,6 +18,8 @@
 
 
 from lib import *
+import math
+	
 
 def get_site_location(site_name,api):
 	
@@ -58,15 +60,112 @@ def site_site_pairs(api,allow_self=True): # allow_self: allow self-referencing p
 			r.append((l[i],l[j]))
 	return r
 
+
+class Site_site_stats:
+	delay_avg = 0.0
+	loss_avg = 0.0
+	delay_stddev = 0.0
+	loss_stddev = 0.0
+	
+	avg_calculated = False
+	
+	cache_site_site_pairs = []
+	pairs = []
+	
+	def __init__(self, api):
+		self.cache_site_site_pairs = site_site_pairs(api,False)
+		for p in self.cache_site_site_pairs:
+			src = p[0]
+			dst = p[1]
+			stats = api.link_statistics(src,dst,"5minutes")
+			
+			#find last entry
+			last = {'end':0}
+			for entry in stats:
+				if entry['end']>last['end']:
+					last = entry
+					
+			self.pairs.append({'src':src,'dst':dst,'laststat':last})
+			
+	def calc_avg(self):
+		links = self.pairs
+			
+		#calculate average
+		delay_sum = 0.0
+		loss_sum = 0.0
+		for l in links:
+			if l['laststat'].has_key('delay_avg') and l['laststat'].has_key('loss'):
+				delay_sum += l['laststat']["delay_avg"]
+				loss_sum += l['laststat']["loss"]
+		self.delay_avg = delay_sum / (len(links) if links else 1.0)
+		self.loss_avg = loss_sum / (len(links) if links else 1.0)
+
+		#calculate stddev
+		delay_stddev = 0.0
+		loss_stddev = 0.0
+		for l in links:
+			if l['laststat'].has_key('delay_avg') and l['laststat'].has_key('loss'):
+				delay_stddev += (self.delay_avg - l['laststat']["delay_avg"]) * (self.delay_avg - l['laststat']["delay_avg"])
+				loss_stddev += (self.loss_avg - l['laststat']["loss"]) * (self.loss_avg - l['laststat']["loss"])
+		if len(links) > 1:
+			delay_stddev /= len(links) -1
+			loss_stddev /= len(links) -1
+		self.delay_stddev = math.sqrt(delay_stddev)
+		self.loss_stddev = math.sqrt(loss_stddev)
+
+		
+	def get_delay_avg(self):
+		if not self.avg_calculated:
+			self.calc_avg()
+		return self.delay_avg
+	
+	def get_loss_avg(self):
+		if not self.avg_calculated:
+			self.calc_avg()
+		return self.loss_avg
+		
+	def get_delay_stddev(self):
+		if not self.avg_calculated:
+			self.calc_avg()
+		return self.delay_stddev
+	
+	def get_loss_stddev(self):
+		if not self.avg_calculated:
+			self.calc_avg()
+		return self.loss_stddev
+	
+	def get_color(self,src,dst): # returns a html-formatted color
+		
+		#find loss/delay for particular link in cache
+		p = None
+		for pa in self.pairs:
+			if (pa['src'] == src and pa['dst'] == dst) or (pa['src'] == dst and pa['dst'] == src):
+				p = pa
+		if p == None or not (p['laststat'].has_key('delay_avg') and p['laststat'].has_key('loss')): #in case nothing was found
+			return '#000000'
+		
+		
+		delay = p['laststat']['delay_avg']
+		loss  = p['laststat']['loss']
+		
+		delay_avg_factor = (delay - self.get_delay_avg()) / self.get_delay_stddev()
+		loss_factor = (loss - self.get_loss_avg()) / self.get_loss_stddev() if loss > 0.001 else -2
+
+		factor = max(delay_avg_factor, loss_factor)
+		factor = max(min((factor+2)/5, 1), 0); #normalize -2..3 -> 0..1
+		return "hsl(" + str((1-factor)/3) + ",1.0,0.4)";
+		
+
 def site_site_connections(api):
 	r = []
-	l = site_site_pairs(api,False)
+	sstats = Site_site_stats(api)
+	l = sstats.cache_site_site_pairs
+	
 	for i in l:
 		src = i[0]
 		dst = i[1]
 		
-		color = "#ffff44"
-		#TODO: select useful color.
+		color = sstats.get_color(src, dst)
 		
 		r.append({
 			"src":src,
