b6a40a54897252368873b15acc268dfe040d4fc2
Tim Gerhard <tim.tg.gerhard@googlemail.com>
2013-03-14 20:41:41 +0100

    finished topology import: importer can now completely restore the topology as given by the file

--- web/tomato/topology_export.py
+++ web/tomato/topology_export.py
@@ -18,15 +18,36 @@
 
 import re
 
+
 #imports a json structure to a topology. returns the new topology's id.
 #return structure:
 # {'success': True, 'id': <new topologies id>}
 # {'success': False, 'message': <error message>}
 def import_topology(api, topology_structure):
     
+    
+    
+    #definition of export file version 1:
+    #
+    #{
+    #    file_info:     { version: 1},
+    #    elements:     [{
+    #                    id: int
+    #                    parent: int <reference to other element> | None
+    #                    type: str <type of element which is understood by backend>
+    #                    attrs: {<attrs for element which are understood by backend>}
+    #                    }]
+    #    connections: [{
+    #                    elements: [ int <reference to an element> ] (length:2)
+    #                    attrs: {<attrs for connection which are understood by backend>}
+    #                    }]
+    #}
+    #
+    #
+    
     def import_v1(api, topo):
         top = topo['topology'].copy()
-        top_id = 0
+        top_id = None
         importer_prefix='_v1importer_'
         
         try:
@@ -44,10 +65,57 @@ def import_topology(api, topology_structure):
                                             attrs = e['attrs'])
                     e[importer_prefix+'id'] = el['id']
                     
-        
-        
+            #add all connectors:
+            for e in top['elements']:
+                if e['parent'] is not None:
+                    
+                    # find reference to recently created parent element:
+                    parent_found = None
+                    for p in top['elements']:
+                        if p['id'] == e['parent']:
+                            parent_found = p[importer_prefix+'id']
+                    if parent_found is None:
+                        api.topology_remove(top_id)
+                        return {'success':False, 'message': 'incomplete references inside elements: "element with ID '+e['parent']+' not found"'}
+                    
+                    el = api.element_create(top_id,
+                                            e['type'],
+                                            parent = parent_found,
+                                            attrs = e['attrs'])
+                    e[importer_prefix+'id'] = el['id']
+                    
+            #add all connections:
+            for c in top['connections']:
+                #find both references to recently created connectors:
+                el_id0 = c['elements'][0]
+                el_id1 = c['elements'][1]
+                conn_id0 = None
+                conn_id1 = None
+                for e in top['elements']:
+                    if e['id'] == el_id0:
+                        conn_id0 = e[importer_prefix+'id']
+                    if e['id'] == el_id1:
+                        conn_id1 = e[importer_prefix+'id']
+                if conn_id0 is None:
+                    api.topology_remove(top_id)
+                    return {'success':False, 'message': 'incomplete references from connections to elements: "element with ID '+el_id0+' not found"'}
+                if conn_id1 is None:
+                    api.topology_remove(top_id)
+                    return {'success':False, 'message': 'incomplete references from connections to elements: "element with ID '+el_id1+' not found"'}
+                    
+                #create the connection
+                conn = api.connection_create(conn_id0,conn_id1)
+                c[importer_prefix+'id'] = conn['id']
+                    
+                    
+            
+        #on error: delete newly created topology:        
         except KeyError as e:
-            return {'success':False, 'message': 'missing key in file structure: ' + e } #"incomplete data structure (KeyError)"}
+            api.topology_remove(top_id)
+            return {'success':False, 'message': 'missing key in file structure: ' + e }
+        except:
+            api.topology_remove(top_id)
+            raise
         
         return {'success':True, 'id':top_id}
     
@@ -55,7 +123,7 @@ def import_topology(api, topology_structure):
     
     
     
-    #check if version info exists
+    #check if version info exists in file:
     if not 'file_information' in topology_structure:
         return {'success': False, 'message': "unknown file structure."}
     inf = topology_structure['file_information']
@@ -87,8 +155,6 @@ def export(api, id):
         
         del data['id']
         del data['permissions']
-        del data['attrs']['_snap_to_grid']
-        del data['attrs']['_fixed_pos']
         
         blacklist = ['capabilities', 
                      'cap_attrs', 
@@ -100,7 +166,18 @@ def export(api, id):
                      'topology',
                      'state']
         
+        blacklist_elements = ['children',
+                              'connection']
+        
+        blacklist_connections = ['id']
+        
+        blacklist_attrs = ['_snap_to_grid',
+                           '_fixed_pos']
+        
         data = reduceData_rec(data, blacklist)
+        data['elements'] = reduceData_rec(data['elements'],blacklist_elements)
+        data['connections'] = reduceData_rec(data['connections'],blacklist_connections)
+        data['attrs'] = reduceData_rec(data['attrs'], blacklist_attrs)
         
         return data
     
