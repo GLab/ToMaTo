7cc9bd59d3ffe73248e83e5dfc5abc2296f68941
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2013-07-16 04:24:31 -0700

    test for fix

--- backend/tomato/elements/kvmqm.py
+++ backend/tomato/elements/kvmqm.py
@@ -18,6 +18,7 @@
 from .. import elements
 import generic, time
 from ..lib import util #@UnresolvedImport
+from django.db.models import Q
 
 class KVMQM(generic.VMElement):
 	TYPE = "kvmqm"
@@ -40,7 +41,8 @@ class KVMQM_Interface(generic.VMInterface):
 		app_label = 'tomato'
 
 def syncRexTFV():
-	for e in KVMQM.objects.filter(next_sync__lte=int(time.time())):
+	for e in KVMQM.objects.filter(next_sync__lte=int(time.time())).filter(~Q(next_sync = 0)):
+		print e
 		e.updateInfo()
 rextfv_syncer = util.RepeatedTimer(1, syncRexTFV)
 #don't forget to start/stop this in tomato/__init__.py
