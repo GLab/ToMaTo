cdaf215072d780ec2185b3c0d1e6e6d189672ce8
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2013-11-22 15:41:44 +0100

    added organizations to webfrontends

--- web/tomato/site.py
+++ web/tomato/site.py
@@ -25,18 +25,31 @@ from admin_common import is_hostManager
 class SiteForm(forms.Form):
     name = forms.CharField(max_length=50, help_text="The name of the site. Must be unique to all sites. e.g.: ukl")
     description = forms.CharField(max_length=255, help_text="e.g.: Technische Universit&auml;t Kaiserslautern")
+    organization = forms.CharField(max_length=50)
     location = forms.CharField(max_length=255, help_text="e.g.: Germany")
     geolocation_longitude = forms.FloatField(help_text="Float Number. >0 if East, <0 if West",label="Geolocation: Longitude")
     geolocation_latitude = forms.FloatField(help_text="Float Number. >0 if North, <0 if South",label="Geolocation: Latitude")
+    def __init__(self, api, *args, **kwargs):
+        super(SiteForm, self).__init__(*args, **kwargs)
+        self.fields["organization"].widget = forms.widgets.Select(choices=organization_name_list(api))
     
 class EditSiteForm(SiteForm):
-    def __init__(self, *args, **kwargs):
-        super(EditSiteForm, self).__init__(*args, **kwargs)
+    def __init__(self, api, *args, **kwargs):
+        super(EditSiteForm, self).__init__(api, *args, **kwargs)
         self.fields["name"].widget=forms.TextInput(attrs={'readonly':'readonly'})
         self.fields["name"].help_text=None
     
 class RemoveSiteForm(forms.Form):
     name = forms.CharField(max_length=50, widget=forms.HiddenInput)
+    
+    
+def organization_name_list(api):
+    l = api.organization_list()
+    res = []
+    for organization in l:
+        res.append((organization["name"],organization["description"] or organization["name"]))
+    res.sort()
+    return res
 
 @wrap_rpc
 def index(api, request):
@@ -45,18 +58,19 @@ def index(api, request):
 @wrap_rpc
 def add(api, request):
     if request.method == 'POST':
-        form = SiteForm(request.POST)
+        form = SiteForm(api, request.POST)
         if form.is_valid():
             formData = form.cleaned_data
             api.site_create(formData["name"],formData["description"])
             api.site_modify(formData["name"],{"location": formData["location"],
                                               'geolocation':{'longitude':formData['geolocation_longitude'],
-                                                             'latitude':formData['geolocation_latitude']}})
+                                                             'latitude':formData['geolocation_latitude']},
+                                              'organization':formData['organization']})
             return render_to_response("admin/site/add_success.html", {'user': api.user, 'name': formData["name"]})
         else:
             return render_to_response("admin/site/form.html", {'user': api.user, 'form': form, "edit":False})
     else:
-        form = SiteForm
+        form = SiteForm(api)
         return render_to_response("admin/site/form.html", {'user': api.user, 'form': form, "edit":False})
     
 @wrap_rpc
@@ -88,13 +102,14 @@ def remove(api, request, name=None):
 @wrap_rpc
 def edit(api, request, name=None):
     if request.method=='POST':
-        form = EditSiteForm(request.POST)
+        form = EditSiteForm(api, request.POST)
         if form.is_valid():
             formData = form.cleaned_data
             api.site_modify(formData["name"],{'description':formData["description"],
                                               'location':formData["location"],
                                               'geolocation':{'longitude':formData['geolocation_longitude'],
-                                                             'latitude':formData['geolocation_latitude']}})
+                                                             'latitude':formData['geolocation_latitude']},
+                                              'organization':formData['organization']})
             return render_to_response("admin/site/edit_success.html", {'user': api.user, 'name': formData["name"]})
         else:
             if not name:
@@ -112,7 +127,7 @@ def edit(api, request, name=None):
             siteInfo['geolocation_longitude'] = siteInfo['geolocation'].get('longitude',0)
             siteInfo['geolocation_latitude'] = siteInfo['geolocation'].get('latitude',0)
             del siteInfo['geolocation']
-            form = EditSiteForm(siteInfo)
+            form = EditSiteForm(api, siteInfo)
             return render_to_response("admin/site/form.html", {'user': api.user, 'name': name, 'form': form, "edit":True})
         else:
             return render_to_response("main/error.html",{'user': api.user, 'type':'not enough parameters','text':'No site specified. Have you followed a valid link?'})
