cf2ffe673964eeb74bf8c90c86416bd4df212a4c
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2013-11-28 10:52:48 +0100

    Added related_name hints

--- hostmanager/tomato/user.py
+++ hostmanager/tomato/user.py
@@ -19,3 +19,7 @@ from django.db import models
 
 class User(models.Model):
 	name = models.CharField(max_length=20, unique=True) #@ReservedAssignment
+	# elements: [Element]
+	# connections: [Connection]
+	# templates: [Template]
+	# networks: [Network]
