ef385ab47938465c5a5d624324a8cbb1ee89c30f
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2013-11-29 09:14:05 +0100

    Forwarding parameters

--- contrib/templates/create_kvm_all.sh
+++ contrib/templates/create_kvm_all.sh
@@ -2,6 +2,6 @@
 
 for arch in i386 amd64; do
   while read dist; do
-    echo -e "$arch\n$dist\n\n\n" | ./create_kvm_template.sh
+    echo -e "$arch\n$dist\n\n\n" | ./create_kvm_template.sh "$@"
   done < distros.build.txt
 done
