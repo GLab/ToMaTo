d3406e4dcac0e253727825779919b560d244d837
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2014-01-08 16:54:18 +0100

    Failing gracefully when templates miss essential attributes

--- backend/tomato/resources/__init__.py
+++ backend/tomato/resources/__init__.py
@@ -24,169 +24,172 @@ from ..lib import db, attributes, util #@UnresolvedImport
 TYPES = {}
 
 def give(type_, num, owner):
-    if isinstance(owner, Element):
-        instance = ResourceInstance.objects.get(type=type_, num=num, ownerElement=owner)
-    elif isinstance(owner, Connection):
-        instance = ResourceInstance.objects.get(type=type_, num=num, ownerConnection=owner)
-    else:
-        fault.raise_("Owner must either be Element or Connection, was %s" % owner.__class__.__name__, fault.INTERNAL_ERROR)
-    instance.delete()
+	if isinstance(owner, Element):
+		instance = ResourceInstance.objects.get(type=type_, num=num, ownerElement=owner)
+	elif isinstance(owner, Connection):
+		instance = ResourceInstance.objects.get(type=type_, num=num, ownerConnection=owner)
+	else:
+		fault.raise_("Owner must either be Element or Connection, was %s" % owner.__class__.__name__, fault.INTERNAL_ERROR)
+	instance.delete()
 
 def take(type_, owner):
-    res = getByType(type_)
-    fault.check(res, "Multiple or none resources of type %s found", type_, fault.INTERNAL_ERROR)
-    range_ = res.getInstanceRange()
-    for try_ in xrange(0, 100): 
-        num = random.choice(range_)
-        try:
-            ResourceInstance.objects.get(type=type_, num=num)
-            continue
-        except ResourceInstance.DoesNotExist:
-            pass
-        try:
-            instance = ResourceInstance()
-            instance.init(type_, num, owner)
-            return num
-        except:
-            pass
-    fault.raise_("Failed to obtain resource of type %s after %d tries" % (type_, try_), code=fault.INTERNAL_ERROR)
+	res = getByType(type_)
+	fault.check(res, "Multiple or none resources of type %s found", type_, fault.INTERNAL_ERROR)
+	range_ = res.getInstanceRange()
+	for try_ in xrange(0, 100): 
+		num = random.choice(range_)
+		try:
+			ResourceInstance.objects.get(type=type_, num=num)
+			continue
+		except ResourceInstance.DoesNotExist:
+			pass
+		try:
+			instance = ResourceInstance()
+			instance.init(type_, num, owner)
+			return num
+		except:
+			pass
+	fault.raise_("Failed to obtain resource of type %s after %d tries" % (type_, try_), code=fault.INTERNAL_ERROR)
 
 from ..elements import Element
 from ..connections import Connection
 
 class Resource(db.ChangesetMixin, attributes.Mixin, models.Model):
-    type = models.CharField(max_length=20, validators=[db.nameValidator], choices=[(t, t) for t in TYPES]) #@ReservedAssignment
-    attrs = db.JSONField()
-    numStart = attributes.attribute("num_start", int)
-    numCount = attributes.attribute("num_count", int)
-    
-    FIELD_NAME = None #take type as field name
-    
-    class Meta:
-        pass
+	type = models.CharField(max_length=20, validators=[db.nameValidator], choices=[(t, t) for t in TYPES]) #@ReservedAssignment
+	attrs = db.JSONField()
+	numStart = attributes.attribute("num_start", int)
+	numCount = attributes.attribute("num_count", int)
+	
+	FIELD_NAME = None #take type as field name
+	
+	class Meta:
+		pass
 
-    def init(self, attrs={}):
-        self.attrs = {}
-        self.save()
-        try:
-            self.modify(attrs)
-        except:
-            self.remove()
-            raise
-        
-    def getInstanceRange(self):
-        fault.check(self.numStart and self.numCount, "This resource can not have any instances", code=fault.INTERNAL_ERROR)
-        return xrange(self.numStart, self.numStart + self.numCount)
-        
-    def upcast(self):
-        if not self.type in TYPES:
-            return self
-        try:
-            field = TYPES[self.type].FIELD_NAME or self.type
-            return getattr(self, field)
-        except:
-            import traceback
-            traceback.print_exc()
-        fault.raise_("Failed to cast resource #%d to type %s" % (self.id, self.type), code=fault.INTERNAL_ERROR)
-    
-    def modify(self, attrs):
-        if not _initPhase:
-            fault.check(currentUser().hasFlag(Flags.GlobalHostManager), "Method only allowed for admin users")        
-        for key, value in attrs.iteritems():
-            if hasattr(self, "modify_%s" % key):
-                getattr(self, "modify_%s" % key)(value)
-            else:
-                self.attrs[key] = value
-        self.save()
-    
-    def modify_num_start(self, val):
-        self.numStart = val
-    
-    def modify_num_count(self, val):
-        self.numCount = val
+	def init(self, attrs={}):
+		self.attrs = {}
+		self.save()
+		try:
+			self.modify(attrs)
+		except:
+			self.remove()
+			raise
+		
+	def getInstanceRange(self):
+		fault.check(self.numStart and self.numCount, "This resource can not have any instances", code=fault.INTERNAL_ERROR)
+		return xrange(self.numStart, self.numStart + self.numCount)
+		
+	def upcast(self):
+		if not self.type in TYPES:
+			return self
+		try:
+			field = TYPES[self.type].FIELD_NAME or self.type
+			return getattr(self, field)
+		except:
+			import traceback
+			traceback.print_exc()
+		fault.raise_("Failed to cast resource #%d to type %s" % (self.id, self.type), code=fault.INTERNAL_ERROR)
+	
+	def modify(self, attrs):
+		if not _initPhase:
+			fault.check(currentUser().hasFlag(Flags.GlobalHostManager), "Method only allowed for admin users")		
+		for key, value in attrs.iteritems():
+			if hasattr(self, "modify_%s" % key):
+				getattr(self, "modify_%s" % key)(value)
+			else:
+				self.attrs[key] = value
+		self.save()
+	
+	def modify_num_start(self, val):
+		self.numStart = val
+	
+	def modify_num_count(self, val):
+		self.numCount = val
 
-    def remove(self):
-        fault.check(currentUser().hasFlag(Flags.GlobalHostManager), "Method only allowed for admin users")
-        self.delete()    
-    
-    def info(self):
-        return {
-            "id": self.id,
-            "type": self.type,
-            "attrs": self.attrs.copy(),
-        }
-    
+	def remove(self):
+		fault.check(currentUser().hasFlag(Flags.GlobalHostManager), "Method only allowed for admin users")
+		self.delete()	
+	
+	def info(self):
+		return {
+			"id": self.id,
+			"type": self.type,
+			"attrs": self.attrs.copy(),
+		}
+	
 class ResourceInstance(db.ChangesetMixin, attributes.Mixin, models.Model):
-    type = models.CharField(max_length=20, validators=[db.nameValidator], choices=[(t, t) for t in TYPES]) #@ReservedAssignment
-    num = models.IntegerField()
-    ownerElement = models.ForeignKey(Element, null=True)
-    ownerConnection = models.ForeignKey(Connection, null=True)
-    attrs = db.JSONField()
-    
-    class Meta:
-        unique_together = (("num", "type"),)
+	type = models.CharField(max_length=20, validators=[db.nameValidator], choices=[(t, t) for t in TYPES]) #@ReservedAssignment
+	num = models.IntegerField()
+	ownerElement = models.ForeignKey(Element, null=True)
+	ownerConnection = models.ForeignKey(Connection, null=True)
+	attrs = db.JSONField()
+	
+	class Meta:
+		unique_together = (("num", "type"),)
 
-    def init(self, type, num, owner, attrs={}): #@ReservedAssignment
-        self.type = type
-        self.num = num
-        if isinstance(owner, Element):
-            self.ownerElement = owner
-        elif isinstance(owner, Connection):
-            self.ownerConnection = owner
-        else:
-            fault.raise_("Owner must either be Element or Connection, was %s" % owner.__class__.__name__, fault.INTERNAL_ERROR)
-        self.attrs = attrs
-        self.save()
+	def init(self, type, num, owner, attrs={}): #@ReservedAssignment
+		self.type = type
+		self.num = num
+		if isinstance(owner, Element):
+			self.ownerElement = owner
+		elif isinstance(owner, Connection):
+			self.ownerConnection = owner
+		else:
+			fault.raise_("Owner must either be Element or Connection, was %s" % owner.__class__.__name__, fault.INTERNAL_ERROR)
+		self.attrs = attrs
+		self.save()
 
 
 def get(id_, **kwargs):
-    try:
-        el = Resource.objects.get(id=id_, **kwargs)
-        return el.upcast()
-    except:
-        return None
+	try:
+		el = Resource.objects.get(id=id_, **kwargs)
+		return el.upcast()
+	except:
+		return None
 
 def getByType(type_, **kwargs):
-    try:
-        el = Resource.objects.get(type=type_, **kwargs)
-        return el.upcast()
-    except:
-        return None
+	try:
+		el = Resource.objects.get(type=type_, **kwargs)
+		return el.upcast()
+	except:
+		return None
 
 def getAll(**kwargs):
-    return (res.upcast() for res in Resource.objects.filter(**kwargs))
+	return (res.upcast() for res in Resource.objects.filter(**kwargs))
 
 def _addResourceRange(type_, start, count):
-    return create(type_, {"num_start": start, "num_count": count})
+	return create(type_, {"num_start": start, "num_count": count})
 
 def create(type_, attrs={}):
-    if not _initPhase:
-        fault.check(currentUser().hasFlag(Flags.GlobalHostManager), "Method only allowed for admin users")    
-    if type_ in TYPES:
-        res = TYPES[type_]()
-    else:
-        res = Resource(type=type_)
-    try:
-        res.init(attrs)
-        res.save()
-    except:
-        res.remove()
-        raise
-    return res
+	if not _initPhase:
+		fault.check(currentUser().hasFlag(Flags.GlobalHostManager), "Method only allowed for admin users")	
+	if type_ in TYPES:
+		res = TYPES[type_]()
+	else:
+		res = Resource(type=type_)
+	try:
+		res.init(attrs)
+		res.save()
+	except:
+		try:
+			res.remove()
+		except:
+			pass
+		raise
+	return res
 
 def init():
-    import profile
-    global _initPhase
-    _initPhase = True
-    for tech in profile.TECHS:
-        try:
-            profile.getPreferred(tech)
-        except:
-            print >>sys.stderr, "Adding default profile for %s" % tech
-            attrs = {"tech": tech, "name": "normal", "label": "Normal", "preference": 10}
-            attrs.update(profile.DEFAULTS[tech])
-            create("profile", attrs)
-    _initPhase = False
+	import profile
+	global _initPhase
+	_initPhase = True
+	for tech in profile.TECHS:
+		try:
+			profile.getPreferred(tech)
+		except:
+			print >>sys.stderr, "Adding default profile for %s" % tech
+			attrs = {"tech": tech, "name": "normal", "label": "Normal", "preference": 10}
+			attrs.update(profile.DEFAULTS[tech])
+			create("profile", attrs)
+	_initPhase = False
 
 _initPhase=False
 
