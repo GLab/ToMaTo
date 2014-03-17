ab55a964bb8b36e65ccd8bf50c7038b43ddaaf8e
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2014-03-17 10:17:24 +0100

    Fixed css problems in tutorials

--- web/tomato/tutorial.py
+++ web/tomato/tutorial.py
@@ -17,12 +17,15 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 
-from lib import wrap_rpc
+from lib import wrap_rpc, security_token
+from admin_common import ConfirmForm
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 import json
-import urllib2
+import urllib2, urllib
 from urlparse import urljoin
 from settings import tutorial_list_url
+
 
 
 #This is a list of available tutorials (values should be self-explaining):
@@ -45,6 +48,11 @@ def list(api, request):
 @wrap_rpc
 def start(api, request):
 	url = request.REQUEST["url"]
+	token = request.REQUEST.get("token")
+	correct_token = security_token(url)
+	if token != correct_token:
+		form = ConfirmForm.build(reverse("tomato.tutorial.start")+"?"+urllib.urlencode({"token": correct_token, "url": url}))
+		return render(request, "form.html", {"heading": "Load tutorial", "message_before": "Are you sure you want to load the tutorial from the following URL? <pre>"+url+"</pre>", 'form': form})
 	_, _, top_dict = loadTutorial(url)
 	top_dict['topology']['attrs']['_tutorial_url'] = url
 	top_dict['topology']['attrs']['_tutorial_status'] = 0
