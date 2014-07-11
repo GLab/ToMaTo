9baa246fc8e9c903dffbfb63a78cc7c37196d461
t-gerhard <t_gerhard10@cs.uni-kl.de>
2014-07-11 13:30:48 +0200

    dumps with the same timestamp now get assigned different IDs. Now using a locking mechanism for access to the dumps dict.

--- shared/lib/dump.py
+++ shared/lib/dump.py
@@ -16,6 +16,8 @@ envCmds = {}
 #environment data is saved to disk loaded on demand if needed.
 #the keys are the dumps IDs, for faster access.
 dumps = {}
+#when adding or removing keys to this array, it has to be locked.
+dumps_lock = threading.RLock()
 
 #structure for dumps:
 # { timestamp:time.Time  # time the error was detected
@@ -52,6 +54,18 @@ def get_absolute_path(dump_id):
     if not filename.ends_with(".json"):
         filename=dump_id+".json"
     return os.path.join(config.DUMP_DIR, filename)
+
+#get a free dump ID
+def get_free_dumpid(timestamp):
+    with dumps_lock:
+        global dumps
+        dump_id = timestamp
+        if not dump_id in dumps:
+            return dump_id
+        i = 0
+        while dump_id+"_"+i in dumps:
+            i+=1
+        return dump_id+"_"+i
     
 #save dump to a file and store it in the dumps dict. return the dump's ID
 def save_dump(timestamp=None, caller=None, **kwargs):
@@ -63,18 +77,21 @@ def save_dump(timestamp=None, caller=None, **kwargs):
     timestr = datetime.strftime(datetime.fromtimestamp(timestamp), timestamp_format)
     if not caller is False:
 		data["caller"] = getCaller()
-    dump_id = timestamp
         
-    #create the dump
-    data = {"environment": getEnv(), "timestamp": timestr, 'dump_id': dump_id}
-    data.update(kwargs)
-    
-    #save it (in the dumps array, and on disk)
-    with open(get_absolute_path(dump_id), "w") as f:
-		json.dump(data, f, indent=2)
-    del data['environment']
-    data['timestamp'] = timestamp
-    dumps[dump_id] = data
+    #we need to lock between choosing an ID and saving it to the array.
+    with dumps_lock:
+        dump_id = get_free_dumpid(timestamp)
+            
+        #create the dump
+        data = {"environment": getEnv(), "timestamp": timestr, 'dump_id': dump_id}
+        data.update(kwargs)
+        
+        #save it (in the dumps array, and on disk)
+        with open(get_absolute_path(dump_id), "w") as f:
+    		json.dump(data, f, indent=2)
+        del data['environment']
+        data['timestamp'] = timestamp
+        dumps[dump_id] = data
     
     return dump_id
     
@@ -93,20 +110,22 @@ def load_dump(dump_id,load_env=False,compress=False,push_to_dumps=False):
         dump['environment'] = zlib.compress(str(dump['environment']),9)
         
     if push_to_dumps:
-        global dumps
-        dump_p = dump
-        if load_env:
-            del dump_p['environment']
-        dumps[dump['dump_id']]=dump_p
+        with dumps_lock:
+            global dumps
+            dump_p = dump
+            if load_env:
+                del dump_p['environment']
+            dumps[dump['dump_id']]=dump_p
         
     return dump
 
 #remove a dump
 def remove_dump(dump_id):
-    global dumps
-    if dump_id in dumps:
-        os.remove(get_absolute_path(dump_id))
-        del dumps[dump_id]
+    with dumps_lock:
+        global dumps
+        if dump_id in dumps:
+            os.remove(get_absolute_path(dump_id))
+            del dumps[dump_id]
         
 #remove all dumps matching a criterion. If criterion is set to None, ignore this criterion.
 #before: remove only if the exception is older than this argument. time.Time object
@@ -117,18 +136,19 @@ def remove_all_where(before=None,excid=None):
     if before is None and excid is None:
         return
     
-    for d in dumps:
-        dump = dumps[d]
-        
-        #for every criterion: if it is not none and does not apply, do not remove the dump file
-        matches_criteria = True
-        if before is not None and before<dump['timestamp_unix']:
-            matches_criteria = False
-        if excid is not None and excid!=dump['excid']:
-            matches_criteria = False
-        
-        if matches_criteria:
-            remove_dump(d)
+    with dumps_lock:
+        for d in dumps:
+            dump = dumps[d]
+            
+            #for every criterion: if it is not none and does not apply, do not remove the dump file
+            matches_criteria = True
+            if before is not None and before<dump['timestamp_unix']:
+                matches_criteria = False
+            if excid is not None and excid!=dump['excid']:
+                matches_criteria = False
+            
+            if matches_criteria:
+                remove_dump(d)
 
 #return the total number of error dumps
 def getCount():
@@ -164,13 +184,14 @@ def dumpException(**kwargs):
     
 #initialize dump management on server startup.
 def init(env_cmds):
-    global envCmds
-    global dumps
-    envCmds = env_cmds
-    
-    if not os.path.exists(config.DUMP_DIR):
-		os.mkdir(config.DUMP_DIR)
-    else:
-        dump_list = os.listdir(config.DUMP_DIR)
-        for d in dump_list:
-            dump = load_dump(d,push_to_dumps=True)
+    with dumps_lock:
+        global envCmds
+        global dumps
+        envCmds = env_cmds
+        
+        if not os.path.exists(config.DUMP_DIR):
+    		os.mkdir(config.DUMP_DIR)
+        else:
+            dump_list = os.listdir(config.DUMP_DIR)
+            for d in dump_list:
+                dump = load_dump(d,push_to_dumps=True)
