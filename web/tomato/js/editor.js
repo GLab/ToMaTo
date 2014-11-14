f0ca1daa1007aea5f12f3b1b299c8ff375bf5391
t-gerhard <t_gerhard10@cs.uni-kl.de>
2014-11-14 13:49:15 +0100

    added doc

--- web/tomato/js/editor.js
+++ web/tomato/js/editor.js
@@ -4070,8 +4070,10 @@ var RexTFV_status_updater = Class.extend({
 		this.options = options;
 		this.elements = [];
 	},
-	updateAll: function(t) { //pass itself to updateAll
+	updateAll: function(t) { //this should be called by a timer. Takes RexTFV_status_updater as argument.
 		toRemove = [];
+		//iterate through all entries, update them, and then update their retry-count according to the refreshed data.
+		//do not remove elements while iterating. instead, put to-be-removed entries in the toRemove array.
 		for (var i=0; i<t.elements.length; i++) {
 			entry = t.elements[i];
 			entry.element.update();
@@ -4084,11 +4086,20 @@ var RexTFV_status_updater = Class.extend({
 				}
 			}
 		}
+		//remove entries marked as to-remove.
 		for (var i=0; i<toRemove.length; i++) {
 			t.remove(toRemove[i]);
 		}
 	},
-	add: function(el,tries) {
+	add: function(el,tries) { //every entry has a number of retries and an element attached to it.
+								// retries are decreased when the status is something else than "running".
+								// retries are set to 1 if the status is "running".
+								// the idea is that the system might need some time to detect RexTFV activity after uploading the archive or starting a device.
+								// retries == 1 is the default. if retries < 0, the entry is removed. This means, after the status is set to "not running",
+								// the editor will update the element twice before removing it.
+		
+		
+		//first, search whether this element is already monitored. If yes, update number of tries if necessary (keep the bigger one). exit funciton if found.
 		for (var i=0; i<this.elements.length; i++) {
 			if (this.elements[i].element == el) {
 				found = true;
@@ -4097,11 +4108,14 @@ var RexTFV_status_updater = Class.extend({
 				return
 			}
 		}
+		
+		//if the search hasn't found anything, simply append this.
 		this.elements.push({
 			element: el,
 			tries: tries
 		})
 	},
+	// usually only called by this. removes an entry.
 	remove: function(entry) {
 		for (var i=0; i<this.elements.length; i++) {
 			entry_found = this.elements[i];
