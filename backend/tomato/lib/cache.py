fd1652335060e2fa73f31bf9198a4cc2893670e4
t-gerhard <t_gerhard10@cs.uni-kl.de>
2014-12-09 11:59:39 +0100

    improved multithreading

--- backend/tomato/lib/cache.py
+++ backend/tomato/lib/cache.py
@@ -19,24 +19,20 @@ import time, sys, threading, thread
 
 class CacheUpdater:
 	caches = []
-	lock = threading.RLock()
 	def add(self, cache):
-		with self.lock:
-			self.caches.append(cache)
+		self.caches.append(cache)
 	def remove(self, cache):
-		with self.lock:
-			self.caches.remove(cache)
+		self.caches.remove(cache)
 	def start_updating(self, interval):
 		from .. import scheduler
 		scheduler.scheduleRepeated(interval, self.update_all, immediate=False)
 	def update_all(self):
-		with self.lock:
-			for cache in self.caches:
-				thread.start_new_thread(cache.update_all)
+		for cache in list(self.caches):
+			cache.update_all()
 cache_updater = None
 
 class Cache:
-	def __init__(self, fn, maxSize=100, timeout=None, autoupdate=False):
+	def __init__(self, fn=None, maxSize=100, timeout=None, autoupdate=False):
 		self._values={} #{key:{value, timeout, auto_timeout, args, kwargs}}
 		self._order=[]  #[key]
 		self._maxSize=maxSize
@@ -50,41 +46,51 @@ class Cache:
 	def getKey(args, kwargs):
 		return (tuple(args), tuple(kwargs.items()))
 	def get(self, args, kwargs):
+		key = Cache.getKey(args, kwargs)
 		with self._lock:
-			key = Cache.getKey(args, kwargs)
 			if (not key in self._values) or (self._values[key]['timeout'] <= time.time()):
 				self.update(args, kwargs)
 			return self._values[Cache.getKey(args, kwargs)]['value']
 	def update(self, args, kwargs):
+		calltime = time.time()
+		value = self._fn(*args, **kwargs) #there may be concurrent method calls here. set() will resolve this.
+		self.set(args, kwargs, value, calltime=calltime)
+	def set(self, args, kwargs, value, calltime=None):
+		key = Cache.getKey(args, kwargs)
+		if calltime is None:
+			calltime = time.time()
 		with self._lock:
-			value = self._fn(*args, **kwargs)
-			self.set(args, kwargs, value)
-	def set(self, args, kwargs, value):
-		with self._lock:
-			key = Cache.getKey(args, kwargs)
+			
+			timeout = (calltime + self._timeout) if self._timeout else sys.maxint
+			auto_timeout = timeout - 0.25*self._timeout if self._timeout else sys.maxint #auto-refresh triggers after 3/4 timeout
+			
+			#check whether another thread has accessed this function concurrently (may happen in update). If yes, do not save this.
+			if key in self._values:
+				if self._values[key]['timeout'] > timeout:
+					return
 			
 			#clear oldest entry if cache is full
 			if len(self._order) >= self._maxSize:
 				delkey = self._order.pop(0)
 				del self._values[delkey]
 				
-			#maintain order of caching
+			#make sure that this key is not in _order (it may only be once in there)
 			if key in self._values:
 				self._order.remove(key)
+				
+			#save
 			self._order.append(key)
-			
-			#save value
-			timeout = (time.time() + self._timeout) if self._timeout else sys.maxint
-			auto_timeout = timeout - 0.25*self._timeout if self._timeout else sys.maxint #auto-refresh triggers after 3/4 timeout
 			self._values[key] = {'value':value,
 								 'timeout':timeout,
 								 'auto_timeout':auto_timeout,
 								 'args':args,
 								 'kwargs':kwargs}
-		if not self._autoupdate_registered:
-			if cache_updater is not None:
-				cache_updater.add(self)
-				self._autoupdate_registered = True
+			
+			#finally, register this cache for auto-update if needed.
+			if not self._autoupdate_registered:
+				if cache_updater is not None:
+					cache_updater.add(self)
+					self._autoupdate_registered = True
 	def remove(self, args, kwargs):
 		with self._lock:
 			key = Cache.getKey(args, kwargs)
@@ -101,25 +107,29 @@ class Cache:
 		if self._autoupdate:
 			for key in list(self._order): #since maxsize is bounded, this is not a scaling problem.
 				with self._lock: #release the lock between all cycles to allow other threads to step in between two iterations.
-								#this function can only change the order of entries, but does not delete any.
-					res = self._values[key]
-					if res['auto_timeout'] <= time.time():
-						self.update(res['args'], res['kwargs'])
+								#    This function can only change the order of entries, but does not delete any.
+								# Concurrent calls may remove the first entries of the list. In this case, this function should ignore these entries.
+								#    However, there is only a really slight chance that this happens, and it doesn't lead to inconsistency.
+								#	 This only means there is no guarantee that the key is still in the list when entering the lock.
+								#        Conclusion: only do something if the key is still cached.
+					if key in self._values:
+						res = self._values[key]
+						if res['auto_timeout'] <= time.time():
+							self.update(res['args'], res['kwargs'])
 		
 		
 	
 class CachedMethod:
-	def __init__(self, fn, cache):
-		self._fn = fn
+	def __init__(self, cache):
 		self._cache = cache
 	def __call__(self, *args, **kwargs):
 		return self._cache.get(args, kwargs)
 	def invalidate(self):
 		self._cache.clear()	
 	
-def cached(timeout=None, cache=None, maxSize=100, autoupdate=False):
+def cached(timeout=None, maxSize=100, autoupdate=False):
 	def wrap(fn):
-		_cache = cache if cache else Cache(fn=fn, timeout=timeout, maxSize=maxSize, autoupdate=autoupdate)
+		_cache = Cache(fn=fn, timeout=timeout, maxSize=maxSize, autoupdate=autoupdate)
 		call = CachedMethod(fn, _cache)
 		call.__name__ = fn.__name__
 		call.__doc__ = fn.__doc__
@@ -129,5 +139,5 @@ def cached(timeout=None, cache=None, maxSize=100, autoupdate=False):
 
 def init():
 	cache_updater = CacheUpdater()
-	cache_updater.start_updating(5*60)
+	cache_updater.start_updating(5*60) #refresh every five minutes. For most caches, this usually means iterating through a list without doing anything.
 	
