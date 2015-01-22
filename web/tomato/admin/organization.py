7daa1a954a4a9caa01f185f295df14f176c64626
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2015-01-22 12:37:24 +0100

    Cleanup

--- web/tomato/admin/organization.py
+++ web/tomato/admin/organization.py
@@ -24,14 +24,12 @@ Created on Dec 4, 2014
 @author: t-gerhard
 '''
 
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
-from django.core.urlresolvers import reverse
 
 from tomato.crispy_forms.layout import Layout
 from ..admin_common import Buttons
-from ..lib import wrap_rpc, AuthError
+from ..lib import wrap_rpc
 from . import add_function, edit_function, remove_function, AddEditForm, RemoveConfirmForm
 
 
