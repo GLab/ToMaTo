26b1543792f9ee7350bf9c6f74791fc8d3082685
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2015-07-08 10:12:16 +0200

    Tracking more files

---
+++ contrib/templates/build_docker_template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+NAME=$1
+OUTPUT=${NAME}_x86_64.tar.gz
+
+if [ "$NAME" == "" ]; then
+  echo "Usage: $0 NAME"
+  exit 1
+fi
+
+if ! [ -d $NAME ]; then
+  echo "Build directory for $NAME does not exist"
+  exit 1
+fi
+
+make prepare_vm.sh
+cp -a prepare_vm.sh $NAME/prepare_vm.sh
+docker build --rm -t $NAME $NAME
+#docker run -it --name $NAME $NAME /bin/false
+#docker export $NAME | gzip > $OUTPUT
+docker run -d --name $NAME $NAME bash -c 'while true; do sleep 1; done'
+ID=$(docker inspect --format='{{.Id}}' $NAME)
+MOUNTDIR=/var/lib/docker/aufs/mnt/$ID
+sudo tar -czf `pwd`/$OUTPUT --numeric-owner -C $MOUNTDIR .
+docker stop $NAME
+docker rm $NAME
