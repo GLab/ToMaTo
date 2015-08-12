22aca6229c5b12d2ba3046805ec8fbc275fd204f
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2015-08-12 11:51:28 +0200

    Fixed repy profile attributes

--- backend/tomato/elements/repy.py
+++ backend/tomato/elements/repy.py
@@ -26,7 +26,7 @@ class Repy(VMElement):
 	CAP_CHILDREN = {
 		"repy_interface": [ST_CREATED, ST_PREPARED],
 	}
-	PROFILE_ATTRS = ["ram", "diskspace", "cpus", "bandwidth"]
+	PROFILE_ATTRS = ["ram", "cpus", "bandwidth"]
 	DIRECT_ACTIONS_EXCLUDE = ["prepare", "destroy"]
 
 	def action_prepare(self):
