7d86943e6818c4f6b9f0b161d33a3bd6f730d45b
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2015-10-16 13:12:09 +0200

    api call durations are not printed to stdout anymore, but are logged in a file (only if enabled)

--- web/tomato/lib/__init__.py
+++ web/tomato/lib/__init__.py
@@ -19,9 +19,12 @@
 from django.http import HttpResponse
 import xmlrpclib, urllib, hashlib
 from . import anyjson as json
+import os
 from .. import settings
 from .error import Error  # @UnresolvedImport
 from .handleerror import renderError, ajaxError, renderFault, ajaxFault
+
+enable_duration_log = False
 
 
 def getauth(request):
@@ -62,6 +65,52 @@ def cached(timeout):
 class AuthError(Exception):
 	pass
 
+class APIDurationLog:
+	entry_count = 100
+	def __init__(self, enabled=True, filename=None):
+		self.enabled = enabled
+		self.filename = filename
+		if enabled:
+			if filename and os.path.exists(filename):
+				try:
+					with open(filename) as f:
+						data = json.loads(f.read())
+						self.durations = data['durations']
+				except:
+					self.durations = {}
+			else:
+				self.durations = {}
+
+	def _save(self):
+		if self.filename:
+			with open(self.filename, 'w+') as f:
+				f.write(json.dumps({
+					'durations': self.durations
+				}))
+
+	def log_call(self, name, duration, args, kwargs):  # fixme: create args/kwargs depending logs
+		if self.enabled:
+			try:
+				drs = self.durations[name]
+			except KeyError:
+				drs = []
+				self.durations[name] = drs
+			drs.append(duration)
+			if len(drs) > APIDurationLog.entry_count:
+				drs.pop(0)
+			self._save()
+
+	def get_api_durations(self):
+		if self.enabled:
+			return {k: {'duration': reduce(lambda x, y: x + y, l) / len(l)} for k, l in self.durations.iteritems()}
+		return {}
+
+
+def api_duration_log():
+	return APIDurationLog(enable_duration_log, '/tmp/tomato-web-api_duration_log.json')  # fixme: allow to configure this. the file should be located in tmpfs or similar.
+
+def log_api_duration(name, duration, args, kwargs):
+	api_duration_log().log_call(name, duration, args, kwargs)
 
 class ServerProxy(object):
 	def __init__(self, url, **kwargs):
@@ -72,13 +121,13 @@ class ServerProxy(object):
 
 		def _call(*args, **kwargs):
 			import time
-			before = time.time()
 			try:
+				before = time.time()
 				# print "%s(%s, %s)" % (name, args, kwargs)
 				res = call_proxy(args, kwargs)
 				after = time.time()
 				# print "%f, %s(%s, %s) -> %s" % (after-before, name, args, kwargs, res)
-				print "%f, %s(%s, %s)" % (after - before, name, args, kwargs)
+				log_api_duration(name, after-before, args, kwargs)
 				return res
 			except xmlrpclib.Fault, e:
 				if e.faultCode == 999:
