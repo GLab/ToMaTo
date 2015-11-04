36999bc8b987a687d9d2194cdaf285fb03ec73a9
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2015-11-04 09:50:43 +0100

    removed package names from cmd params

--- docker/run/cmds.sh
+++ docker/run/cmds.sh
@@ -63,7 +63,7 @@ function tomato-db-stop () {
 
 function tomato-backend-start () {
   mkdir -p "$DIR"/backend/{config,data,logs}
-  tomato-write_version_info "$DIR"/backend/config tomato-backend
+  tomato-write_version_info "$DIR"/backend/config
   docker run -d -v "$TOMATODIR"/backend:/code -v "$TOMATODIR"/shared:/shared \
              -v "$DIR"/backend/config:/config -v "$DIR"/backend/data:/data -v "$DIR"/backend/logs:/logs \
              --link mongodb:db -p 8006:8006 -p 8000:8000 -p 8001:8001 -p 8002:8002 \
@@ -75,7 +75,7 @@ function tomato-backend-start () {
 
 function tomato-backend-shell () {
   mkdir -p "$DIR"/backend/{config,data,logs}
-  tomato-write_version_info "$DIR"/backend/config tomato-backend
+  tomato-write_version_info "$DIR"/backend/config
   docker run -it --rm -v "$TOMATODIR"/backend:/code -v "$TOMATODIR"/shared:/shared \
              -v "$DIR"/backend/config:/config -v "$DIR"/backend/data:/data -v "$DIR"/backend/logs:/logs \
              --link mongodb:db -p 8006:8006 -p 8000:8000 -p 8001:8001 -p 8002:8002 \
@@ -100,7 +100,7 @@ function tomato-backend-restart() {
 
 function tomato-web-start () {
   mkdir -p "$DIR"/web/config
-  tomato-write_version_info "$DIR"/web/config tomato-web
+  tomato-write_version_info "$DIR"/web/config
   SECRET_KEY=$(cat /dev/urandom | tr -cd 'a-zA-Z0-9' | head -c 32)
   docker run -d -v "$TOMATODIR"/web:/code -v "$TOMATODIR"/shared:/shared -v "$DIR"/web/config:/config \
              --link tomato-backend:backend -p 8080:80 --dns 131.246.9.116 --name tomato-web \
