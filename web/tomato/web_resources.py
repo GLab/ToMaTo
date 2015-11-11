cd9278abb7ca1327f2fe0a2ede82f401a46fbfde
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2015-11-11 15:26:16 +0100

    webfrontend can now show a list of default archives

--- web/tomato/web_resources.py
+++ web/tomato/web_resources.py
@@ -3,6 +3,9 @@ __author__ = 't-gerhard'
 import json, urllib2
 from urlparse import urljoin
 from settings import default_executable_archives_list_url
+from django.shortcuts import render
+import time
+from .lib import wrap_rpc
 from .lib.reference_library import techs
 
 def web_resources():
@@ -10,8 +13,7 @@ def web_resources():
 		'executable_archives': executable_archives()
 	}
 
-
-def executable_archives():
+def executable_archives(split_alternatives=True):
 	default_archive_list = []
 	default_archive_list_imported = json.load(urllib2.urlopen(default_executable_archives_list_url))
 	for default_archive_listentry in default_archive_list_imported:
@@ -31,25 +33,93 @@ def executable_archives():
 			entry['icon'] = None
 
 		entry['description'] = default_archive.get('description', None)
+		entry['creation_date'] = default_archive.get('creation_date', None)
+		if entry['creation_date']:
+			entry['creation_date'] = time.mktime(time.strptime(entry['creation_date'], '%Y-%m-%d'))
 
 		entry['default_archive'] = urljoin(url, default_archive['default_archive'])
 
-		entry['alternatives'] = {k: dict() for k in techs()}
+		if split_alternatives:
+			entry['alternatives'] = {k: dict() for k in techs()}
+		else:
+			entry['alternatives'] = []
+
 		for alternative in default_archive.get('alternatives', []):
 			assert 'templates' in alternative
 			assert 'archive' in alternative
-			for template in alternative['templates']:
-				alt_entry = {
-					'url': urljoin(url, alternative['archive'])
-				}
-				alt_entry['description'] = alternative.get('description', None)
-				if ':' in template:
-					tech, template_name = template.split(':')
-					entry['alternatives'][tech][template_name] = alt_entry
-				else:
-					for tech in techs():
-						entry['alternatives'][tech][template] = alt_entry
+			alt_entry = {
+				'url': urljoin(url, alternative['archive']),
+				'creation_date': alternative.get('creation_date', None)
+			}
+			alt_entry['description'] = alternative.get('description', None)
+			if alt_entry['creation_date']:
+				alt_entry['creation_date'] = time.mktime(time.strptime(alt_entry['creation_date'], '%Y-%m-%d'))
+			else:
+				alt_entry['creation_date'] = entry['creation_date']
+
+			if split_alternatives:
+				for template in alternative['templates']:
+						if ':' in template:
+							tech, template_name = template.split(':')
+							entry['alternatives'][tech][template_name] = alt_entry
+						else:
+							for tech in techs():
+								entry['alternatives'][tech][template] = alt_entry
+			else:
+				alt_entry['templates'] = alternative['templates']
+				entry['alternatives'].append(alt_entry)
+
 
 		default_archive_list.append(entry)
 
 	return default_archive_list
+
+
+
+def executable_archive_list(request):
+	archives = executable_archives(False)
+	for archive in archives:
+		archive['alternatives'] = len(archive['alternatives'])
+	return render(request, 'web_resources/default_executable_archive_list.html', {'archive_list': archives, 'default_executable_archives_list_url': default_executable_archives_list_url})
+
+@wrap_rpc
+def executable_archive_info(api, request, name):
+	archives = executable_archives(False)
+
+	# find archive corresponding to name
+	archive = None
+	for archive_ in archives:
+		if archive_['name'] == name:
+			archive = archive_
+	if not archive:
+		return  # fixme: return 404
+
+	# build template dict for later usage
+	template_list = api.template_list()
+	template_dict = {k: dict() for k in techs()}
+	for template in template_list:
+		template_dict[template['tech']][template['name']] = template
+
+	# convert template list into better format
+	alt_list_new = []
+	for alternative in archive['alternatives']:
+		templates_described = []
+		for template in alternative['templates']:
+			if ':' in template:
+				tech, template_name = template.split(':')
+				templates_described.append((tech, template_name))
+			else:
+				for tech in techs():
+					templates_described.append((tech, template))
+		alternative['templates'] = []
+		for tech, name in templates_described:
+			template = template_dict.get(tech, {}).get(name, None)
+			if template:
+				alternative['templates'].append({
+					'id': template['id'],
+					'name': name,
+					'label': template['label'] if template['label'] else name,
+					'tech': tech
+				})
+
+	return render(request, 'web_resources/default_executable_archive_info.html', {'archive': archive, 'default_executable_archives_list_url': default_executable_archives_list_url})
