30cea132f7ff77f077011fd8f89977e3a9909243
Dennis Schwerdel <schwerdel@informatik.uni-kl.de>
2016-02-01 09:28:01 +0100

    Not using cache when building docker images so that most current packages get installed

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -4,5 +4,5 @@ set -e
 
 NAME="$1"
 VERSION=$(./getversion.sh "$NAME/Dockerfile")
-docker build --rm -t "$NAME:$VERSION" "$NAME"
+docker build --rm --no-cache -t "$NAME:$VERSION" "$NAME"
 docker tag -f "$NAME:$VERSION" "$NAME:latest"
