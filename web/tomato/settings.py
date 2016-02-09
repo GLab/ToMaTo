8d4537548d2fe2798f01a0aa251520877d2e2215
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-02-09 16:57:14 +0100

    webfrontend no longer uses old config files

--- web/tomato/settings.py
+++ web/tomato/settings.py
@@ -100,17 +100,7 @@ INSTALLED_APPS = (
 	'tomato'
 )
 
-server_protocol = "http"
-server_host = "localhost"
-server_port = "8000"
-if "BACKEND_PORT_8000_TCP" in os.environ:
-	server_host = os.getenv('BACKEND_PORT_8000_TCP_ADDR')
-	server_port = os.getenv('BACKEND_PORT_8000_TCP_PORT')
-
 server_httprealm = "G-Lab ToMaTo"
-
-tutorial_list_url = "http://packages.tomato-lab.org/tutorials/index.json"
-default_executable_archives_list_url = ""
 
 CRISPY_TEMPLATE_PACK = "bootstrap3"
 
@@ -121,38 +111,7 @@ if DJANGO_VERSION < (1, 4):
 	)
 	SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
-# access tokens can be created via GitHub settings -> My Personal Access Tokens
-# The token needs the scope 'repo' or 'public_repo' (depending on whether the repository is private or not)
-github_access_token = ""
-github_repository_owner = "GLab"
-github_repository_name = "ToMaTo"
 
-# enable API duration log.
-# this slows down the webfrontend due to disk read/write operations after each API call.
-# in exchange, it accurately logs the average duration of each API call.
-enable_duration_log = False
-# for better performance, the log should be placed on a fast disk (or even tmpfs or similar).
-# there is no reason for this log to be persistent.
-duration_log_location = "/tmp/webfrontend_api_duration_log.json"
-# the log saves the last n durations. A higher n means more protection against deviations.
-# however, when actively debugging, this means that it takes longer for changes to become visible.
-# this problem can be skipped by clearing the log file before testing a new change.
-duration_log_size = 25
-
-# specify how often user information is updated (seconds between updates).
-# a longer interval improves performance for webfrontend and backend,
-# but it means that it takes some time for user account changes to be applied.
-# updates are only done when the user opens a page which uses the API.
-account_info_update_time = 120
-
-try:
-	import sys
-	for path in filter(os.path.exists, ["/etc/tomato/web.conf", os.path.expanduser("~/.tomato/web.conf"), "web.conf"]):
-		try:
-			execfile(path)
-			print >> sys.stderr, "Loaded config from %s" % path
-		except Exception, exc:
-			print >> sys.stderr, "Failed to load config from %s: %s" % (path, exc)
-except:
-	import traceback
-	traceback.print_exc()
+import sys
+for path in filter(os.path.exists, ["/etc/tomato/web.conf", os.path.expanduser("~/.tomato/web.conf"), "web.conf"]):
+	print >> sys.stderr, "Found old-style config at %s - This is no longer supported." % (path)
