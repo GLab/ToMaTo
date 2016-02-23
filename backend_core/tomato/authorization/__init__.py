925613a9bf8150c2fa328929ca25aeee7f5c4b90
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-02-23 15:23:02 +0100

    class hierarchy for info objects in auth

--- backend_core/tomato/authorization/__init__.py
+++ backend_core/tomato/authorization/__init__.py
@@ -1,25 +1,9 @@
-from ..lib.service import get_tomato_inner_proxy
-from ..lib.settings import Config
 from ..lib.userflags import Flags
-from .. import scheduler
+from info import UserInfo
 
-class UserInfo:
+class PermissionChecker(UserInfo):
 	def __init__(self, username):
-		self.name = username
-		self.api = get_tomato_inner_proxy(Config.TOMATO_MODULE_BACKEND_USERS)
-		self._info = None
-
-	def invalidate_info(self):
-		self._info = None
-
-	def info(self):
-		if self._info is None:
-			self._info = self.api.user_info(self.name)
-			scheduler.scheduleOnce(60, self.invalidate_info)  # fixme: invalidation interval should be configurable
-		return self._info
-
-	def get_username(self):
-		return self.name
+		super(PermissionChecker, self).__init__(username)
 
 	def may_list_all_users(self):
 		"""
@@ -29,7 +13,7 @@ class UserInfo:
 		:rtype: bool
 		"""
 		# only global admins may do this.
-		return Flags.GlobalAdmin in self.info()['flags']
+		return Flags.GlobalAdmin in self.get_flags()
 
 	def may_list_organization_users(self, organization):
 		"""
@@ -39,9 +23,9 @@ class UserInfo:
 		:rtype: bool
 		"""
 		# global admins and admins of the respective organization may do this.
-		if Flags.GlobalAdmin in self.info()['flags']:
+		if Flags.GlobalAdmin in self.get_flags():
 			return True
-		if Flags.OrgaAdmin in self.info()['flags'] and self.info()['organization'] == organization:
+		if Flags.OrgaAdmin in self.get_flags() and self.get_organization() == organization:
 			return True
 		return False
 
@@ -53,10 +37,10 @@ class UserInfo:
 		:rtype: list(str)
 		"""
 		res = {'name', 'origin', 'id', 'realname'}  # needed for everyone to add a user to a topology.
-		if self.name == userB.get_username():
+		if self.get_username() == userB.get_username():
 			res.update('email', 'flags', 'organization', 'quota', 'client_data', 'last_login', 'password_hash')
-		if Flags.GlobalAdmin in self.info()['flags'] or \
-				Flags.OrgaAdmin in self.info()['flags'] and self.info()['organization'] == userB.info()['organization']:
+		if Flags.GlobalAdmin in self.get_flags() or \
+				Flags.OrgaAdmin in self.get_flags() and self.get_organization() == userB.get_organization():
 			res.update('email', 'flags', 'organization', 'quota', 'notification_count', 'client_data', 'last_login', 'password_hash')
 		return res
 
@@ -71,14 +55,14 @@ class UserInfo:
 		:rtype: list(str)
 		"""
 		result = {"flags"}
-		if userB.get_username() == self.name:
+		if userB.get_username() == self.get_username():
 			result.update(("realname", "email", "password"))
-			if Flags.GlobalAdmin in self.info()['flags']:
+			if Flags.GlobalAdmin in self.get_flags():
 				result.add("organization")
 		else:
-			if Flags.GlobalAdmin in self.info()['flags']:
+			if Flags.GlobalAdmin in self.get_flags():
 				result.update("realname", "email", "organization")
-			if Flags.OrgaAdmin in self.info()['flags'] and self.info['organization'] == userB.info()['organization']:
+			if Flags.OrgaAdmin in self.get_flags() and self.info['organization'] == userB.get_organization():
 				result.update("realname", "email")
 		return result
 
@@ -90,14 +74,14 @@ class UserInfo:
 		:rtype: bool
 		"""
 		result = set()
-		is_self = userB.get_username() == self.name
+		is_self = userB.get_username() == self.get_username()
 
 		# every user may change their own email settings
 		if is_self:
 			result.add(Flags.NoMails)
 
 		# all global flags, and OrgaAdmin flag
-		if Flags.GlobalAdmin in self.info()['flags']:
+		if Flags.GlobalAdmin in self.get_flags():
 			if not is_self:
 				result.add(Flags.GlobalAdmin)  # may not remove own admin privileges
 			result.update([
@@ -113,8 +97,8 @@ class UserInfo:
 						])
 
 		# all flags except global flags.
-		if Flags.GlobalAdmin in self.info()['flags'] or \
-				(Flags.OrgaAdmin in self.info()['flags'] and self.info()['organization'] == userB.info()['organization']):
+		if Flags.GlobalAdmin in self.get_flags() or \
+				(Flags.OrgaAdmin in self.get_flags() and self.get_organization() == userB.get_organization()):
 			if not is_self:
 				result.add(Flags.OrgaAdmin)  # may not remove own admin privileges. if GlobalAdmin, this has been added before.
 			result.update([
@@ -142,11 +126,11 @@ class UserInfo:
 		:return: whether this user may reset B's password
 		:rtype: bool
 		"""
-		if userB.get_username() == self.name:
+		if userB.get_username() == self.get_username():
 			return True
-		if Flags.GlobalAdmin in self.info()['flags']:
+		if Flags.GlobalAdmin in self.get_flags():
 			return True
-		if Flags.OrgaAdmin in self.info()['flags'] and self.info()['organization'] == userB.info()['organization']:
+		if Flags.OrgaAdmin in self.get_flags() and self.get_organization() == userB.get_organization():
 			return True
 		return False
 
@@ -157,14 +141,14 @@ class UserInfo:
 		:return: whether this user may delete userB
 		:rtype: bool
 		"""
-		if userB.get_username() == self.name:
-			if Flags.GlobalAdmin in self.info()['flags']:  # admins may not delete themselves
+		if userB.get_username() == self.get_username():
+			if Flags.GlobalAdmin in self.get_flags():  # admins may not delete themselves
 				return False
 			return True
-		if Flags.GlobalAdmin in self.info()['flags']:
+		if Flags.GlobalAdmin in self.get_flags():
 			return True
-		if Flags.OrgaAdmin in self.info()['flags'] and self.info()['organization'] == userB.info()['organization']:
-			if Flags.GlobalAdmin in userB.info()['flags']:
+		if Flags.OrgaAdmin in self.get_flags() and self.get_organization() == userB.get_organization():
+			if Flags.GlobalAdmin in userB.get_flags():
 				return False  # orgaAdmin may not delete globalAdmin
 			return True
 		return False
@@ -176,10 +160,10 @@ class UserInfo:
 		:return: whether the user may broadcast messages to the given organization or all
 		:rtype: bool
 		"""
-		flags = self.info()['flags']
+		flags = self.get_flags()
 		if Flags.GlobalAdmin in flags or Flags.GlobalHostManager in flags:
 			return True
-		if (Flags.OrgaAdmin in flags or Flags.OrgaHostManager in flags) and self.info()['organization'] == organization:
+		if (Flags.OrgaAdmin in flags or Flags.OrgaHostManager in flags) and self.get_organization() == organization:
 			return True
 		return False
 
@@ -190,11 +174,11 @@ class UserInfo:
 		:return: whether this user may send a message to userB
 		:rtype: bool
 		"""
-		if userB.get_username() == self.name:
+		if userB.get_username() == self.get_username():
 			return True
-		if Flags.GlobalAdmin in self.info()['flags']:
+		if Flags.GlobalAdmin in self.get_flags():
 			return True
-		if Flags.OrgaAdmin in self.info()['flags'] and self.info()['organization'] == userB.info()['organization']:
+		if Flags.OrgaAdmin in self.get_flags() and self.get_organization() == userB.get_organization():
 			return True
 		return False
 
---
+++ backend_core/tomato/authorization/info.py
@@ -0,0 +1,41 @@
+from .. import scheduler
+from ..lib.error import InternalError
+from ..lib.service import get_tomato_inner_proxy
+from ..lib.settings import Config
+
+class InfoObj(object):
+	def __init(self, cache_duration):
+		self._cache_duration = cache_duration
+		self._info = None
+
+	def invalidate_info(self):
+		self._info = None
+
+	def _fetch_data(self):
+		raise InternalError(code=InternalError.UNKNOWN, message="this function should have been overridden", data={'function': '%s._fetch_data' % repr(self.__class__)})
+
+	def info(self):
+		if self._info is None:
+			self._info = self._fetch_data()
+			scheduler.scheduleOnce(self._cache_duration, self.invalidate_info)
+		return self._info
+
+
+
+
+class UserInfo(InfoObj):
+	def __init__(self, username):
+		super(UserInfo, self).__init__(60)  # fixme: invalidation interval should be configurable
+		self.name = username
+
+	def _fetch_data(self):
+		return get_tomato_inner_proxy(Config.TOMATO_MODULE_BACKEND_USERS).user_info(self.name)
+
+	def get_username(self):
+		return self.name
+
+	def get_flags(self):
+		return self.info()['flags']
+
+	def get_organization(self):
+		return self.info()['organization']
