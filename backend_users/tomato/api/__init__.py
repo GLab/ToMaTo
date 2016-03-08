50ad1148c5828bb7a9ad1e1c145f2f80f432e66a
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-03-08 14:15:23 +0100

    selective api method import for backend_users

--- backend_users/tomato/api/__init__.py
+++ backend_users/tomato/api/__init__.py
@@ -9,8 +9,14 @@ def dummy():
 	# todo: this should be obsolete...
 	return "Hello world!"
 
-from auth import *
-from dump import *
-from notification import *
-from organization import *
-from user import *
+from auth import user_check_password
+
+from dump import dump_count, dump_info, dump_list
+
+from notification import notification_get, notification_list, notification_set_read, send_message, broadcast_message
+
+from organization import organization_remove, organization_modify, organization_list,\
+	organization_info, organization_create, organization_exists
+
+from user import user_create, user_exists, user_info, user_list, user_modify, user_modify_password,\
+	user_remove, username_list
