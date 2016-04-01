9d4c56791020f1ec7b0428482839cdd5325f54ee
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-04-01 15:49:06 +0200

    fixes

--- backend_api/tomato/api/misc.py
+++ backend_api/tomato/api/misc.py
@@ -19,7 +19,7 @@ from ..lib.versioninfo import getVersionStr
 from api_helpers import checkauth, getCurrentUserInfo, getCurrentUserName
 from ..lib.service import get_backend_core_proxy, is_self, get_tomato_inner_proxy
 from ..lib.util import joinDicts
-from .. import misc
+from ..misc import getCAPublicKey
 
 def server_info():
 	"""
@@ -32,7 +32,7 @@ def server_info():
 	topology_config = settings.get_topology_settings()
 	return {
 		"TEMPLATE_TRACKER_URL": core_info["TEMPLATE_TRACKER_URL"],
-		'public_key': misc.getCAPublicKey(),
+		'public_key': getCAPublicKey(),
 		'version': getVersionStr(),
 		'api_version': [4, 0, 1],
 		'topology_timeout': {
