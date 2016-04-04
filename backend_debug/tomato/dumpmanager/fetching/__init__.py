0d210ebe1eef35d6439f45b5d1608d30755c3a46
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-04-04 15:29:55 +0200

    better fix for the problem before

--- backend_debug/tomato/dumpmanager/fetching/__init__.py
+++ backend_debug/tomato/dumpmanager/fetching/__init__.py
@@ -3,25 +3,32 @@ import backend as fetching_backend
 import host as fetching_host
 from ...lib.settings import Config
 import time
+from ...lib.error import InternalError
 
 
 def get_all_dumpsources():
 	sources = []
 
-	try:
-		host_name_list = get_backend_core_proxy().host_name_list()
-	except:
+	# step one: fetch list of all hosts' names
+	host_name_list = []
+	if not is_reachable(Config.TOMATO_MODULE_BACKEND_CORE):
 		# backend_core may be restarting, especially if started simultaneously with backend_debug...
-		# give it some time
-		if not is_reachable(Config.TOMATO_MODULE_BACKEND_CORE):
-			time.sleep(10)
+		# give it some time.
+		time.sleep(10)
+	else:
+		try:
+			# if this hasn't been solved by now, ignore all hosts for this fetching.
+			# next time, this will be run again.
 			host_name_list = get_backend_core_proxy().host_name_list()
-			# if this isn't solved after a second retry, raise the error...
-		else:
-			raise
+		except Exception as exc:
+			InternalError(code=InternalError.UNKNOWN, message="Failed to retrieve host list for dump fetching: %s" % exc,
+			              data={"exception": repr(exc)}).dump()
+
+	# step two: convert hosts' names into dump sources
 	for h in host_name_list:
 		sources.append(fetching_host.HostDumpSource(h))
 
+	# step three: add backend modules
 	for mod in Config.TOMATO_BACKEND_INTERNAL_REACHABLE_MODULES:
 		sources.append(fetching_backend.BackendDumpSource(mod))
 
