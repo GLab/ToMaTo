27dd0439f18ddaf644c57b5c042df23be4aca336
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-04-04 15:24:09 +0200

    fixed startup dump throwing bug

--- backend_debug/tomato/dumpmanager/fetching/__init__.py
+++ backend_debug/tomato/dumpmanager/fetching/__init__.py
@@ -1,13 +1,25 @@
-from ...lib.service import get_backend_core_proxy
+from ...lib.service import get_backend_core_proxy, is_reachable
 import backend as fetching_backend
 import host as fetching_host
 from ...lib.settings import Config
+import time
 
 
 def get_all_dumpsources():
 	sources = []
 
-	for h in get_backend_core_proxy().host_name_list():
+	try:
+		host_name_list = get_backend_core_proxy().host_name_list()
+	except:
+		# backend_core may be restarting, especially if started simultaneously with backend_debug...
+		# give it some time
+		if not is_reachable(Config.TOMATO_MODULE_BACKEND_CORE):
+			time.sleep(10)
+			host_name_list = get_backend_core_proxy().host_name_list()
+			# if this isn't solved after a second retry, raise the error...
+		else:
+			raise
+	for h in host_name_list:
 		sources.append(fetching_host.HostDumpSource(h))
 
 	for mod in Config.TOMATO_BACKEND_INTERNAL_REACHABLE_MODULES:
