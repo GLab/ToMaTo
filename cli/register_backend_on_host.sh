aee3871aac91efe5f116bf6e6c0bda5815cbbc0f
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-04-26 18:46:08 +0200

    testing script now registers auto-generated backend on host

--- cli/register_backend_on_host.sh
+++ cli/register_backend_on_host.sh
@@ -11,5 +11,5 @@ print server_info()["public_key"]
 ' > /tmp/register_backend_on_host__extract_key.py
 
 ./tomato.py --url $1 -f /tmp/register_backend_on_host__extract_key.py > /tmp/register_backend_on_host__key.pem
-scp /tmp/register_backend_on_host__key.pem $2:/etc/tomato/client_certs/$(hostname)__autoreg.pem
-ssh $2 update-tomato-client-certs
+scp /tmp/register_backend_on_host__key.pem root@$2:/etc/tomato/client_certs/$(hostname)__autoreg.pem
+ssh root@$2 update-tomato-client-certs
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# stop tomato
-echo -n "stopping current ToMaTo instance... "
-../docker/run/tomato-ctl.py stop > /dev/null
-echo "done"
-
 # read the docker dir from local tomato-ctl.conf
 DOCKER_DIR=$(echo '
 import json
 with open("tomato-ctl.conf") as f:
  config = json.load(f)
 print config["docker_dir"]' | python -)
+
+# read host list
+HOSTS=$(echo '
+import json
+with open("testhosts.json") as f:
+ hosts = json.load(f)
+print " ".join(hosts)' | python -)
+
+# stop tomato
+echo -n "stopping current ToMaTo instance... "
+../docker/run/tomato-ctl.py stop > /dev/null
+echo "done"
 
 # start tomato
 echo -n "starting test ToMaTo instance in $DOCKER_DIR ... "
@@ -25,6 +32,11 @@ sleep 20
 echo "done"
 
 # todo: test which checks reachability of all tomato modules
+
+# copy backend's key to hosts
+for host in ${HOSTS}; do
+	../cli/register_backend_on_host.sh http+xmlrpc://localhost:8000 $host
+done
 
 # run tests
 echo ""
