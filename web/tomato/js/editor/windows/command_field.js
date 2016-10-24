941603af9b2119e0b644f059af14a43094b028b9
Tim Gerhard <t_gerhard10@cs.uni-kl.de>
2016-10-24 14:41:14 +0200

    added command field

---
+++ web/tomato/js/editor/windows/command_field.js
@@ -0,0 +1,11 @@
+function commandField(cmd) {
+	var id_cmd = "commandfield"+cmd.split(" ")[0]; //fixme: only supports one commandfield per command. better conversion
+	return '<div class="input-group" style="width:100%;">'+
+	'<input type="text" class="form-control" id="'+id_cmd+'" readonly style="background-color: white; width:100%; cursor:initial; font-family:monospace;" value="'+cmd+'" />'+
+	'<span class="input-group-btn">'+
+	'<button class="btn btn-secondary" onclick="'+"$('#"+id_cmd+"').select(); document.execCommand('copy'); this.innerHTML='copied!'"+'">'+
+	'copy'+
+	'</button>'+
+	'</span>'+
+	'</div>';
+}
